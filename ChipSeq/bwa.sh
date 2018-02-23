20b31cf1162f6b31ca9560ee13973bce9b5f9c26
Zhang, Feng <jumphone@163.com>
2018-02-23 10:42:33 -0500

    Update bwa.sh

--- ChipSeq/bwa.sh
+++ ChipSeq/bwa.sh
@@ -2,8 +2,8 @@ cpu=$1
 refgenome=$2
 treat=$3
 bwa='/home/zhangfeng/tools/bwa-0.7.12/bwa'
-samtools='/home/zhangfeng/tools/samtools-1.2/samtools'
+samtools='/home/zhangfeng/disk/project/SPRINT_bioinfo_compare/softwares/samtools-1.4/samtools'
 $bwa mem  -M -t $cpu $refgenome $treat > $treat\.sam
 $samtools view -bS $treat\.sam > $treat\.bam
-$samtools sort $treat\.bam $treat\.bam.sorted
-$samtools rmdup $treat\.bam.sorted.bam $treat\.bam.sorted.dedup.bam
+$samtools sort $treat\.bam > $treat\.sorted.bam
+$samtools rmdup $treat\.sorted.bam $treat\.sorted.dedup.bam
