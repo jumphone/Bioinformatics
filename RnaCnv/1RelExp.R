6b9ffef60eb34542710168ecef39cc05806719d3
Zhang, Feng <jumphone@163.com>
2018-03-09 10:48:20 -0500

    Update 1RelExp.R

--- RnaCnv/1RelExp.R
+++ RnaCnv/1RelExp.R
@@ -1,1 +1,15 @@
+TPM_MATRIX="RANDOM200TPM.txt"
 
+
+
+suppressPackageStartupMessages(library(edgeR))
+scale_center=function(x){ return(scale(x,center=T,scale=F)) }
+raw_exp_data=read.delim(TPM_MATRIX, sep='\t', header=T, row.names=1)
+tmm=edgeR::calcNormFactors(raw_exp_data)
+exptmm=edgeR::cpm(raw_exp_data, lib.size = tmm * colSums(raw_exp_data))
+logexp <- log2(exptmm + 1)
+agg=apply(logexp,1,sum)
+gene_analyzed= which( agg > mean(agg))
+exp_data=logexp[gene_analyzed,]
+REL_EXP=t(apply(exp_data,1,scale_center))
+write.table(REL_EXP,'REL_EXP.txt',sep='\t',row.names=T,col.names=T,quote=F)
