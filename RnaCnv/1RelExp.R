1d88c7f7aec64d29fdfe19f6cf43b9529a0527fe
Zhang, Feng <jumphone@163.com>
2018-03-09 13:37:45 -0500

    Update 1RelExp.R

--- RnaCnv/1RelExp.R
+++ RnaCnv/1RelExp.R
@@ -1,16 +1,19 @@
-TPM_MATRIX="RANDOM200TPM.txt"
+TPM_MATRIX="NSC_merge_fpkm_named.txt.pure"
+
+
 
 
 
 suppressPackageStartupMessages(library(edgeR))
-scale_center=function(x){ return(scale(x,center=T,scale=F)) }
+#scale_center=function(x){ return(scale(x,center=T,scale=F)) }
 raw_exp_data=read.delim(TPM_MATRIX, sep='\t', header=T, row.names=1)
 tmm=edgeR::calcNormFactors(raw_exp_data)
 exptmm=edgeR::cpm(raw_exp_data, lib.size = tmm * colSums(raw_exp_data))
-#logexp <- log2(exptmm + 1)
-agg=apply(exptmm,1,sum)
-gene_analyzed= which( agg > quantil(agg,0.8))
+logexp <- log2(exptmm + 1)
+
+agg=apply(logexp,1,sum)
+gene_analyzed= which( agg > quantile(agg,0.0))
 exp_data=exptmm[gene_analyzed,]
-REL_EXP=t(apply(exp_data,1,scale_center))
+REL_EXP=t(apply(exp_data,1,scale))
 colnames(REL_EXP)=colnames(raw_exp_data)
-write.table(REL_EXP,'REL_EXP.txt',sep='\t',row.names=T,col.names=T,quote=F)
+write.ta
