92c2e04f07c3fc58bac9f7f3c1e2c197885dd2c8
Zhang, Feng <jumphone@163.com>
2018-03-09 12:56:39 -0500

    Update 6Draw.R

--- RnaCnv/6Draw.R
+++ RnaCnv/6Draw.R
@@ -1,21 +1,38 @@
 ALLCNV=as.matrix(read.table('relative_ALLCNV.txt',row.names=1,header=T,sep='\t' ))
 CELL_COLOR=as.character(read.table('CELL_COLOR.txt',header=F,sep='\t' )[,1])
 CHROM_COLOR=as.character(read.table('CHROM_COLOR.txt',header=F,sep='\t' )[,1])
+CHROM_TAG=as.factor(read.table('CHROM_TAG.txt',header=F,sep='\t' )[,1])
 
 
 
 ALLCNV[which(ALLCNV >3 )]=3
 ALLCNV[which(ALLCNV < -3 )]= -3
 
-pdf('HEATMAP.pdf',width=30,height=7)
+pdf('CNV.pdf',width=40,height=40)
 
+par(mfrow=c(length(ALLCNV[,1]),1))
 
-library(gplots)
-heatmap.2( ALLCNV,scale = c("none"),col=colorRampPalette(c("blue", "white","red"))(100),
-       Rowv=F,Colv=F,dendrogram='none',trace='none',labCol=F,labRow=F, RowSideColors=CELL_COLOR, ColSideColors=CHROM_COLOR)
+i=1
+while(i<=length(ALLCNV[,1])){
 
-heatmap.2( ALLCNV,scale = c("none"),col=colorRampPalette(c("blue", "white","red"))(100),
-       Rowv=T,Colv=F,dendrogram='none',trace='none',labCol=F,labRow=F, RowSideColors=CELL_COLOR, ColSideColors=CHROM_COLOR)
+boxplot(as.numeric(ALLCNV[i,])~CHROM_TAG,ylim=c(-3,3),pch=16,main=rownames(ALLCNV)[i])
+	i=i+1
+}
+
+par(mfrow=c(length(ALLCNV[,1]),1))
+
+i=1
+while(i<=length(ALLCNV[,1])){
+
+plot(as.numeric(ALLCNV[i,]),col=CHROM_COLOR,ylim=c(-3,3),pch=16,main=rownames(ALLCNV)[i])
+	i=i+1
+}
+#library(gplots)
+#heatmap.2( ALLCNV,scale = c("none"),col=colorRampPalette(c("blue", "white","red"))(100),
+#       Rowv=F,Colv=F,dendrogram='none',trace='none',labCol=F,labRow=F, RowSideColors=CELL_COLOR, ColSideColors=CHROM_COLOR)
+
+#heatmap.2( ALLCNV,scale = c("none"),col=colorRampPalette(c("blue", "white","red"))(100),
+#       Rowv=T,Colv=F,dendrogram='none',trace='none',labCol=F,labRow=F, RowSideColors=CELL_COLOR, ColSideColors=CHROM_COLOR)
 
 
 dev.off()
