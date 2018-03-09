9145c1347667024b26a164e1846ceddc22be0a66
Zhang, Feng <jumphone@163.com>
2018-03-09 11:58:50 -0500

    Update 1RelExp.R

--- RnaCnv/1RelExp.R
+++ RnaCnv/1RelExp.R
@@ -12,4 +12,5 @@ agg=apply(logexp,1,sum)
 gene_analyzed= which( agg > mean(agg))
 exp_data=logexp[gene_analyzed,]
 REL_EXP=t(apply(exp_data,1,scale_center))
+colnames(REL_EXP)=colnames(raw_exp_data)
 write.table(REL_EXP,'REL_EXP.txt',sep='\t',row.names=T,col.names=T,quote=F)
