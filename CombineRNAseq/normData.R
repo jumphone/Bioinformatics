de7dbb9f379fed5b91ed127f068ef4b53c6976b6
Zhang, Feng <jumphone@163.com>
2018-03-26 14:38:42 -0400

    Update normData.R

--- CombineRNAseq/normData.R
+++ CombineRNAseq/normData.R
@@ -1,6 +1,7 @@
 TPM_MATRIX='COMBINED.txt'
 suppressPackageStartupMessages(library(edgeR))
-scale_center=function(x){ return(scale(x,center=T,scale=F)) }
+
+
 raw_exp_data=read.delim(TPM_MATRIX, sep='\t', header=T, row.names=1)
 tmm=edgeR::calcNormFactors(raw_exp_data)
 exptmm=edgeR::cpm(raw_exp_data, lib.size = tmm * colSums(raw_exp_data))
