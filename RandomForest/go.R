6383ae24a43f14f4027ab85b0a095735c5f32115
Zhang, Feng <jumphone@163.com>
2018-04-02 19:53:09 -0400

    Update go.R

--- RandomForest/go.R
+++ RandomForest/go.R
@@ -3,8 +3,18 @@
 library(randomForest)
 rawdata=read.table('OMIMID_Name.txt.stat.name.sig.t',header=T,row.names=1)
 trawdata=t(rawdata)
-index <- sample(2,nrow(iris),replace = TRUE,prob=c(0.7,0.3))
 
+TAG=read.table('OMIMID_Name.txt.stat.name.sig.t.tag',header=T)[,1]
 
+set.seed(12345)
+index <- sample(2,nrow(trawdata),replace = TRUE,prob=c(0.7,0.3))
+traindata <- trawdata[index==1,]
+testdata <- trawdata[index==2,]
+
+TARGET=rep(0,length(traindata[,1]))
+TARGET[which(TAG=='OMIM220210')]=1
+
+set.seed(12345)
+rf_ntree <- randomForest(TARGET~.,data=traindata,ntree=300)
 
 
