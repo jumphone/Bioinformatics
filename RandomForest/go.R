5df5d02a7c3fb95fdbc765576f6658fc45cb7179
Zhang, Feng <jumphone@163.com>
2018-04-02 20:58:23 -0400

    Update go.R

--- RandomForest/go.R
+++ RandomForest/go.R
@@ -5,19 +5,25 @@ library(randomForest)
 rawdata=read.table('OMIMID_Name.txt.stat.name.sig.t',header=T,row.names=1)
 trawdata=t(rawdata)
 
-TAG=read.table('OMIMID_Name.txt.stat.name.sig.t.tag',header=F)[,1]
-TARGET_STR=TAG
-TARGET_NUM=as.numeric(as.factor(TAG))
+TAG=read.table('OMIMID_Name.txt.stat.name.sig.t.tag',header=FALSE)[,1]
+TARGET=as.character(TAG) #as.factor(TAG)
 
-DATA=cbind(trawdata,TARGET_NUM)
+
+#DATA=data.frame(trawdata)
+#DATA$TARGET=TARGET
 
 set.seed(12345)
 index <- sample(2,nrow(DATA),replace = TRUE,prob=c(0.7,0.3))
-traindata <- DATA[index==1,]
-testdata <- DATA[index==2,]
+traindata <- trawdata[index==1,]
+testdata <- trawdata[index==2,]
+
+traintarget <- TARGET[index==1]
+traintarget=as.factor(traintarget)
+
+testtarget <- TARGET[index==2]
 
 set.seed(12345)
-rf_ntree <- randomForest(TARGET_NUM~.,data=traindata,ntree=100,proximity=TRUE)
+rf_ntree <- randomForest(traintarget~. ,data=traindata,ntree=100,proximity=TRUE)
 
 pdf('tmp.pdf')
 plot(rf_ntree)
