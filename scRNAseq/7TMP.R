a7da5692b2ffd7ae42eeb7e91255f2024a73eabe
Zhang, Feng <jumphone@163.com>
2018-05-04 17:24:33 -0400

    Update 7TMP.R

--- scRNAseq/7TMP.R
+++ scRNAseq/7TMP.R
@@ -8,3 +8,10 @@ OUT=as.matrix(EXP_cluster@data[vargene_row,])
 write.table(file='var_gene_data.txt',OUT,sep='\t',quote=F,row.names=T,col.names=T)
 
 
+
+
+library(Seurat)
+library(dplyr)
+library(Matrix)
+a=read.table('var_gene_data.SSN.txt',header=T,row.names=1)
+
