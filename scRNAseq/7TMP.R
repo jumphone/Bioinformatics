daaf502fdb4069b145fb26af7f5e860b4251b52b
Zhang, Feng <jumphone@163.com>
2018-05-04 11:03:27 -0400

    Create 7TMP.R

---
+++ scRNAseq/7TMP.R
@@ -0,0 +1,10 @@
+library(Seurat)
+library(dplyr)
+library(Matrix)
+load("./images/Seurat_EXP_TSNE.Robj")
+load("./images/Seurat_EXP_cluster.Robj")
+vargene_row=which(row.names(EXP_cluster@data) %in% EXP_cluster@var.genes)
+OUT=as.matrix(EXP_cluster@data[vargene_row,])
+write.table('var_gene_data.txt',OUT,sep='\t',quote=F,row.names=T,col.names=T)
+
+
