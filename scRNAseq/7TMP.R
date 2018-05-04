9b39371c9ee22354e980aa4401cc940e64d2a5ba
Zhang, Feng <jumphone@163.com>
2018-05-04 11:04:17 -0400

    Update 7TMP.R

--- scRNAseq/7TMP.R
+++ scRNAseq/7TMP.R
@@ -5,6 +5,6 @@ load("./images/Seurat_EXP_TSNE.Robj")
 load("./images/Seurat_EXP_cluster.Robj")
 vargene_row=which(row.names(EXP_cluster@data) %in% EXP_cluster@var.genes)
 OUT=as.matrix(EXP_cluster@data[vargene_row,])
-write.table('var_gene_data.txt',OUT,sep='\t',quote=F,row.names=T,col.names=T)
+write.table(file='var_gene_data.txt',OUT,sep='\t',quote=F,row.names=T,col.names=T)
 
 
