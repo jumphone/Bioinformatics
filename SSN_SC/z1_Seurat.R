cac501fb045227a9dab8cf93a17111cdc3766592
Zhang, Feng <jumphone@163.com>
2018-05-16 17:02:51 -0400

    Update z1_Seurat.R

--- SSN_SC/z1_Seurat.R
+++ SSN_SC/z1_Seurat.R
@@ -31,9 +31,35 @@ new_data=EXP@data * FAC[,2]
 EXP@scale.data=new_data
 all_gene=row.names(EXP@data)
 PCNUM=20
+PCUSE=1:20
+RES=0.3
+
 EXP <- RunPCA(object = EXP, pc.genes = all_gene, do.print = TRUE, pcs.print = 1:5,    genes.print = 5, pcs.compute=PCNUM, maxit = 500, weight.by.var = FALSE )
+EXP <- RunTSNE(object = EXP, dims.use = PCUSE, do.fast = TRUE, check_duplicates = FALSE)
+EXP_cluster <- FindClusters(object = EXP, reduction.type = "pca", dims.use = PCUSE,  resolution = RES, print.output = 0, save.SNN = TRUE)
+
+pdf('TF_SSN_Seurat.pdf',width=30,height=15)
+TSNEPlot(object = EXP)
+TSNEPlot(object = EXP_cluster)
+TSNEPlot(object = EXP_cluster,do.label=T)
+plot1=TSNEPlot(object = EXP,do.return = T,colors.use=c('grey','grey','grey','grey','grey','red'),plot.order=c('S1.5MD','S1.5MP','S1.5MSN','S4MD','S4MP','S4MSN'))
+plot2=TSNEPlot(object = EXP,do.return = T,colors.use=c('grey','grey','grey','grey','grey','red'),plot.order=c('S1.5MP','S1.5MD','S1.5MSN','S4MD','S4MP','S4MSN'))
+plot3=TSNEPlot(object = EXP,do.return = T,colors.use=c('grey','grey','grey','grey','grey','red'),plot.order=c('S1.5MSN','S1.5MD','S1.5MP','S4MD','S4MP','S4MSN'))
+plot4=TSNEPlot(object = EXP,do.return = T,colors.use=c('grey','grey','grey','grey','grey','red'),plot.order=c('S4MD','S1.5MP','S1.5MSN','S1.5MD','S4MP','S4MSN'))
+plot5=TSNEPlot(object = EXP,do.return = T,colors.use=c('grey','grey','grey','grey','grey','red'),plot.order=c('S4MP','S1.5MD','S1.5MSN','S4MD','S1.5MP','S4MSN'))
+plot6=TSNEPlot(object = EXP,do.return = T,colors.use=c('grey','grey','grey','grey','grey','red'),plot.order=c('S4MSN','S1.5MD','S1.5MP','S4MD','S4MP','S1.5MSN'))
+plot_grid(plot1, plot2,plot3,plot4,plot5,plot6)
+dev.off()
 
 
+
+
+pdf('PCA_A.pdf')
+PCElbowPlot(object = EXP, num.pc = 20)
+PrintPCA(object = EXP, pcs.print = 1:20)
+PCHeatmap(object = EXP, pc.use = 1:10,100)
+PCHeatmap(object = EXP, pc.use = 11:20,100)
+dev.off()
 
 
 new_exp_data = -log(exp_data,10)
