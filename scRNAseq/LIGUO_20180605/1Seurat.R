9a1cbb57544cce4aeca4f0764c137762f4162d22
Zhang, Feng <jumphone@163.com>
2018-06-05 11:49:14 -0400

    Update 1Seurat.R

--- scRNAseq/LIGUO_20180605/1Seurat.R
+++ scRNAseq/LIGUO_20180605/1Seurat.R
@@ -1,13 +1,13 @@
 library(Seurat)
 
 PCNUM=40
-PCUSE=1:35
+PCUSE=1:20
 RES=0.8
 
 exp_data=read.table('run1642_10000.dge.txt',header=T,row.names=1)
 EXP = CreateSeuratObject(raw.data = exp_data, min.cells = 3, min.genes=200)
 
-mito.genes <- grep(pattern = "^Mt", x = rownames(x = EXP@data), value = TRUE)
+mito.genes <- grep(pattern = "^mt-", x = rownames(x = EXP@data), value = TRUE)
 percent.mito <- colSums(EXP@data[mito.genes, ]) / colSums(EXP@data)
 EXP <- AddMetaData(object = EXP, metadata = percent.mito, col.name = "percent.mito")
 
@@ -24,7 +24,7 @@ EXP=FilterCells(object = EXP, subset.names = c("nGene", "percent.mito"), low.thr
 EXP=NormalizeData(object = EXP, normalization.method = "LogNormalize", scale.factor = 10000)
 
 pdf('Seurat_VarGene.pdf')
-EXP <- FindVariableGenes(object = EXP, mean.function = ExpMean, dispersion.function = LogVMR, x.low.cutoff =0, y.cutoff = 0.8)
+EXP <- FindVariableGenes(object = EXP, mean.function = ExpMean, dispersion.function = LogVMR, x.low.cutoff =0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
 dev.off()
 
 length(x=EXP@var.genes)
@@ -52,18 +52,34 @@ length(x=EXP@var.genes)
 
 PrintPCA(object = EXP, pcs.print = 1:5, genes.print = 5, use.full = FALSE)
 
+
+Stem_gene=c('Prom1','Nes','Egfr','Cd15','Slc1a3','Sox2','Fabp7','Nr2e1','Id3','Clu','Sox9','Vcam1','Slc1a2','Id2','Sox11','Apoe','Tbr2','Ntsr2')
+
 EXP = ScaleData(object = EXP,vars.to.regress = c("percent.mito", "nUMI"), genes.use = EXP@var.genes)
 EXP <- RunPCA(object = EXP, pc.genes = EXP@var.genes, do.print = TRUE, pcs.print = 1:5,    genes.print = 5, pcs.compute=PCNUM, maxit = 500, weight.by.var = FALSE )
 
+EXP = ScaleData(object = EXP,vars.to.regress = c("percent.mito", "nUMI"), genes.use = Stem_gene)
+EXP <- RunPCA(object = EXP, pc.genes = Stem_gene, do.print = TRUE, pcs.print = 1:5,    genes.print = 5, pcs.compute=PCNUM, maxit = 500, weight.by.var = FALSE )
+
+
+PCNUM=14
+PCUSE=1:14
+RES=0.8
 PCElbowPlot(object = EXP,num.pc=PCNUM)
 
 PrintPCA(object = EXP, pcs.print = 1:20)
-EXP <- RunTSNE(object = EXP, dims.use = PCUSE, do.fast = TRUE)
+
+
+PCUSE=1:5
+EXP <- RunTSNE(object = EXP, dims.use = PCUSE, do.fast = TRUE, check_duplicates = FALSE)
 #EXP_cluster <- FindClusters(object = EXP, reduction.type = "pca", dims.use = PCUSE,  resolution = RES, print.output = 0, save.SNN = TRUE)
 
 
-FeaturePlot(object = EXP, features.plot = c('Sox2','Egfr','Id3','Slc1a2','Sox11','Apoe','Gfap','Slc1a3','Nes'), cols.use = c("grey", "red"), reduction.use = "tsne")
+FeaturePlot(object = EXP, features.plot = c('Sox2','Egfr','Id3','Olig2','Zic1','Apoe','Gfap','Slc1a3','Nes'), cols.use = c("grey", "red"), reduction.use = "tsne")
 FeaturePlot(object = EXP, features.plot = c('Sox2'),cols.use = c("grey", "red"), reduction.use = "tsne")
+
+
+#FeaturePlot(object = EXP, features.plot = c('Cd133'),cols.use = c("grey", "red"), reduction.use = "tsne")
 
 save(EXP, file = "EXP.Robj")
 save(EXP_cluster, file = "EXP_cluster.Robj")
