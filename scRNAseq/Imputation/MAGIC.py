dd27b6a5e35f7750cbcb7de4c97a7c13c2dac1d1
Zhang, Feng <jumphone@163.com>
2018-07-12 18:02:27 -0400

    Update MAGIC.py

--- scRNAseq/Imputation/MAGIC.py
+++ scRNAseq/Imputation/MAGIC.py
@@ -3,6 +3,9 @@ import pandas as pd
 magic_operator = magic.MAGIC()
 X = pd.read_csv("input.txt",sep='\t')
 
-X_magic = magic_operator.fit_transform(X, genes=['A2M'])
+#X_magic = magic_operator.fit_transform(X, genes=['A2M'])
 
 
+X_magic = magic_operator.fit_transform(X, genes='all_genes')
+
+X_magic.to_csv('output.csv',sep='\t')
