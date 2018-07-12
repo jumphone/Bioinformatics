0fb0731634320472e9316b70ca866daa55c6c332
Zhang, Feng <jumphone@163.com>
2018-07-12 17:32:30 -0400

    Update and rename MAGIC.R to MAGIC.py

--- scRNAseq/Imputation/MAGIC.R
+++
@@ -1,6 +0,0 @@
-library(Rmagic)
-library(ggplot2)
-library(readr)
-library(viridis)
-
-
---
+++ scRNAseq/Imputation/MAGIC.py
@@ -0,0 +1,8 @@
+import magic
+import pandas as pd
+magic_operator = magic.MAGIC()
+X = pd.read_csv("input.txt",sep='\t')
+
+X_magic = magic_operator.fit_transform(X, genes=['A2M'])
+
+
