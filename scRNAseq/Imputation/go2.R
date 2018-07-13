f9b5d6206286c2c7e557f14c0af8701a2eae36a4
Zhang, Feng <jumphone@163.com>
2018-07-13 12:30:48 -0400

    Update go2.R

--- scRNAseq/Imputation/go2.R
+++ scRNAseq/Imputation/go2.R
@@ -9,9 +9,9 @@ exp_data=t(exp_data)
 pbmc <- CreateSeuratObject(raw.data = exp_data, min.cells = 0, min.genes = 0, project = "DIPG")
 
 
-mito.genes <- grep(pattern = "^MT-", x = rownames(x = pbmc@data), value = TRUE)
-percent.mito <- Matrix::colSums(pbmc@raw.data[mito.genes, ])/Matrix::colSums(pbmc@raw.data)
-pbmc <- AddMetaData(object = pbmc, metadata = percent.mito, col.name = "percent.mito")
+#mito.genes <- grep(pattern = "^MT-", x = rownames(x = pbmc@data), value = TRUE)
+#percent.mito <- Matrix::colSums(pbmc@raw.data[mito.genes, ])/Matrix::colSums(pbmc@raw.data)
+#pbmc <- AddMetaData(object = pbmc, metadata = percent.mito, col.name = "percent.mito")
 
 #pdf('QC.pdf',width=10,height=6)
 #VlnPlot(object = pbmc, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
@@ -37,8 +37,8 @@ PCAPlot(object = pbmc, dim.1 = 3, dim.2 = 4)
 PCElbowPlot(object = pbmc)
 dev.off()
 
-pbmc <- FindClusters(object = pbmc, reduction.type = "pca", dims.use = 1:4, resolution = 0.2, print.output = 0, save.SNN = TRUE)
-pbmc <- RunTSNE(object = pbmc, dims.use = 1:4, do.fast = TRUE)
+pbmc <- FindClusters(object = pbmc, reduction.type = "pca", dims.use = 1:10, resolution = 0.2, print.output = 0, save.SNN = TRUE)
+pbmc <- RunTSNE(object = pbmc, dims.use = 1:10, do.fast = TRUE)
 
 pdf('TSNE.pdf')
 TSNEPlot(object = pbmc,do.label = TRUE)
