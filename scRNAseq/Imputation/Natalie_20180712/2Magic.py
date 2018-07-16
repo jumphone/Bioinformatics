711f2926215e95158b6e15a4510bf102ba8cfa53
Zhang, Feng <jumphone@163.com>
2018-07-16 10:51:21 -0400

    Create 2Magic.py

---
+++ scRNAseq/Imputation/Natalie_20180712/2Magic.py
@@ -0,0 +1,20 @@
+import magic
+import numpy as np
+import pandas as pd
+
+bmmsc_data = magic.io.load_csv('MATRIX.txt')
+libsize = bmmsc_data.sum(axis=1)
+
+bmmsc_data = magic.preprocessing.library_size_normalize(bmmsc_data)
+bmmsc_data = np.sqrt(bmmsc_data)
+bmmsc_data.head()
+
+
+magic_op = magic.MAGIC(t=2, k=3)
+#magic_op = magic.MAGIC()
+bmmsc_magic = magic_op.fit_transform(bmmsc_data, genes='all_genes')
+bmmsc_magic.head()
+
+
+bmmsc_magic.to_csv('magic.csv',sep='\t')
+
