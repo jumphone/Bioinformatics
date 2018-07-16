01a361f2a8cd2dbf8c16ac4e1e71d942b0122040
Zhang, Feng <jumphone@163.com>
2018-07-16 11:11:58 -0400

    Update 1QC.R

--- scRNAseq/Imputation/Natalie_20180712/1QC.R
+++ scRNAseq/Imputation/Natalie_20180712/1QC.R
@@ -1,14 +1,15 @@
 library(Seurat)
 library(dplyr)
 
-GENE_CUTOFF_LOW = 200
-GENE_CUTOFF_HIGH = 3000
+GENE_CUTOFF_LOW = 100
+GENE_CUTOFF_HIGH = 1000
 CELL_CUTOFF_LOW = 10
 MITO_CUTOFF_HIGH = 0.1
 
 
 pbmc.data <- Read10X(data.dir = "./raw_gene_bc_matrices/hg19/")
 pbmc <- CreateSeuratObject(raw.data = pbmc.data, min.cells = CELL_CUTOFF_LOW, min.genes = GENE_CUTOFF_LOW,  project = "Human")
+dim(pbmc@raw.data)
 
 mito.genes <- grep(pattern = "^MT-", x = rownames(x = pbmc@data), value = TRUE)
 percent.mito <- Matrix::colSums(pbmc@raw.data[mito.genes, ])/Matrix::colSums(pbmc@raw.data)
@@ -24,10 +25,11 @@ dev.off()
 
 pbmc <- FilterCells(object = pbmc, subset.names = c("nGene", "percent.mito"), low.thresholds = c(GENE_CUTOFF_LOW, -Inf), high.thresholds = c(GENE_CUTOFF_HIGH, MITO_CUTOFF_HIGH))
 
-#dim(pbmc@data)
+dim(pbmc@data)
 #[1] 10346  1483
 
 
 save(pbmc, file = "Seurat.Robj")
 output = t(as.matrix(pbmc@data))
 write.table(output,file='MATRIX.txt',quote=F,sep=',',row.names=T,col.names=T)
+
