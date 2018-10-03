14c545be42e27eba66e06e9748096fa0233b850c
Zhang, Feng <jumphone@163.com>
2018-10-03 17:17:51 -0400

    Update go.R

--- PCA/go.R
+++ PCA/go.R
@@ -1,9 +1,12 @@
 library(Seurat)
 library(dplyr)
 library(Matrix)
+library(htmlwidgets)
+library(plotly)
 
 
 exp_data=read.table('data.txt.rmdup',header=T,row.names=1)
+##########################################
 
 EXP = CreateSeuratObject(raw.data = exp_data, min.cells = 0, min.genes=0)
 
@@ -15,8 +18,35 @@ EXP=NormalizeData(object = EXP, normalization.method = "LogNormalize", scale.fac
 
 EXP = ScaleData(object = EXP,  genes.use = allgene)
 
-EXP <- RunPCA(object = EXP, pc.genes = EXP@var.genes, do.print = TRUE, pcs.print = 1:5,    genes.print = 5, pcs.compute=PCNUM, maxit = 500, weight.by.var = FALSE )
+PCNUM=3
+EXP <- RunPCA(object = EXP, pc.genes = allgene, do.print = TRUE, pcs.print = 1:3,    genes.print = 5, pcs.compute=PCNUM, maxit = 500, weight.by.var = FALSE )
 
-pdf('PCA.pdf',width=15,height=15)
-PCAPlot(object = EXP, dim.1 = 1, dim.2 = 2)
+OUT=PCAPlot(object = EXP, dim.1 = 1, dim.2 = 2, do.label=F, no.legend =T, do.hover = T)
+htmlwidgets::saveWidget(OUT, file = "index.html")
+
+##########################################
+
+
+exp_data=read.table('data.txt.rmdup',header=T,row.names=1,check.names =F)
+exp_data[which(is.na(exp_data))]=0
+exp_data_scale=apply(exp_data,1,scale)
+exp_data_sum=apply(exp_data,1,sum)
+exp_data_var=apply(exp_data,1,var)
+exp_data_scale_t=t(exp_data_scale)
+rownames(exp_data_scale_t)=rownames(exp_data)
+colnames(exp_data_scale_t)=colnames(exp_data)
+exp_data_scale_t=exp_data_scale_t[which(exp_data_var>0),]
+exp_data_scale_t=as.matrix(exp_data_scale_t)
+exp_data_scale_t[which(is.na(exp_data_scale_t))]=0
+exp_data_scale_t_pca=princomp(exp_data_scale_t)
+set.seed(123)
+
+PC1=exp_data_scale_t_pca$loadings[,1]
+PC2=exp_data_scale_t_pca$loadings[,2]
+PC1=jitter(PC1,factor=50)
+PC2=jitter(PC2,factor=50)
+LABEL=rownames(exp_data_scale_t_pca$loadings)
+pdf('PCA.pdf',width=7,height=7)
+plot(PC1,PC2,pch=16, cex=3,col=c('green3','green3','red','red','black','black','blue','blue'))
+text(PC1, PC2, labels=LABEL, cex= 0.7, pos=c(1,2,3,4))
 dev.off()
