85e5eabc886502d30dc4c5c97770e5abb700eb92
Zhang, Feng <jumphone@163.com>
2018-10-15 14:33:32 -0400

    Update go.R

--- scRNAseq/RecLig/go.R
+++ scRNAseq/RecLig/go.R
@@ -3,15 +3,15 @@ library(dplyr)
 library(Matrix)
 library(gplots)
 
-
+load('Seurat_EXP_mb3076_PCA.Robj')
 OUT='exp_LigRec_detail.txt'
 
 RL=read.table('ReceptorLigand.txt.mouse',header=T,sep='\t')
 
-load('Seurat_EXP_mb3076_PCA.Robj')
+this_data=mb3076@data
+this_data=apply(this_data,1,scale)
 
-this_data=EXP@data
-this_CLUSTER=as.character(EXP_cluster@ident)
+this_CLUSTER=as.character(mb3076@ident)
 CNUM=3
 
 
