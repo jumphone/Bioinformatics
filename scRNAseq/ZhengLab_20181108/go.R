8a1bb6b3b61aa4139b01a976eaa1a208021bab58
Zhang, Feng <jumphone@163.com>
2018-11-08 13:31:41 -0500

    Update go.R

--- scRNAseq/ZhengLab_20181108/go.R
+++ scRNAseq/ZhengLab_20181108/go.R
@@ -1,1 +1,35 @@
+#/ZhengLab/10X_20181030/OUT
+library(Seurat)
+
+
+WT291_1.data <- Read10X(data.dir = "./10X_291_WT_20180925_3mm_1_out/raw_gene_bc_matrices/mm10/")
+WT291_2.data <- Read10X(data.dir = "./10X_291_WT_20180925_3mm_2_out/raw_gene_bc_matrices/mm10/")
+WT291_3.data <- Read10X(data.dir = "./10X_291_WT_20180925_3mm_3_out/raw_gene_bc_matrices/mm10/")
+WT291_4.data <- Read10X(data.dir = "./10X_291_WT_20180925_3mm_4_out/raw_gene_bc_matrices/mm10/")
+
+KO294_1.data <- Read10X(data.dir = "./10X_294_KO_20180925_3mm_1_out/raw_gene_bc_matrices/mm10/")
+KO294_2.data <- Read10X(data.dir = "./10X_294_KO_20180925_3mm_2_out/raw_gene_bc_matrices/mm10/")
+KO294_3.data <- Read10X(data.dir = "./10X_294_KO_20180925_3mm_3_out/raw_gene_bc_matrices/mm10/")
+KO294_4.data <- Read10X(data.dir = "./10X_294_KO_20180925_3mm_4_out/raw_gene_bc_matrices/mm10/")
+
+
+WT291_1=CreateSeuratObject(raw.data = WT291_1.data, min.cells = 0, min.genes = 0,  project = "ZhengLab")
+WT291_2=CreateSeuratObject(raw.data = WT291_2.data, min.cells = 0, min.genes = 0,  project = "ZhengLab")
+WT291_3=CreateSeuratObject(raw.data = WT291_3.data, min.cells = 0, min.genes = 0,  project = "ZhengLab")
+WT291_4=CreateSeuratObject(raw.data = WT291_4.data, min.cells = 0, min.genes = 0,  project = "ZhengLab")
+
+
+KO294_1=CreateSeuratObject(raw.data = KO294_1.data, min.cells = 0, min.genes = 0,  project = "ZhengLab")
+KO294_2=CreateSeuratObject(raw.data = KO294_2.data, min.cells = 0, min.genes = 0,  project = "ZhengLab")
+KO294_3=CreateSeuratObject(raw.data = KO294_3.data, min.cells = 0, min.genes = 0,  project = "ZhengLab")
+KO294_4=CreateSeuratObject(raw.data = KO294_4.data, min.cells = 0, min.genes = 0,  project = "ZhengLab")
+
+EXP = MergeSeurat(WT291_1, WT291_2, project = NULL, min.cells = 0, min.genes = 0, is.expr = 0, do.normalize = FALSE, do.scale = FALSE, do.center = FALSE, names.field = 1,names.delim = "_", add.cell.id1 = 'WT1', add.cell.id2 = 'WT2')
+EXP = MergeSeurat(EXP, WT291_3, project = NULL, min.cells = 0, min.genes = 0, is.expr = 0, do.normalize = FALSE, do.scale = FALSE, do.center = FALSE, names.field = 1,names.delim = "_", add.cell.id1 = NULL, add.cell.id2 = 'WT3')
+EXP = MergeSeurat(EXP, WT291_4, project = NULL, min.cells = 0, min.genes = 0, is.expr = 0, do.normalize = FALSE, do.scale = FALSE, do.center = FALSE, names.field = 1,names.delim = "_", add.cell.id1 = NULL, add.cell.id2 = 'WT4')
+
+EXP = MergeSeurat(EXP, KO294_1, project = NULL, min.cells = 0, min.genes = 0, is.expr = 0, do.normalize = FALSE, do.scale = FALSE, do.center = FALSE, names.field = 1,names.delim = "_", add.cell.id1 = NULL, add.cell.id2 = 'KO1')
+EXP = MergeSeurat(EXP, KO294_2, project = NULL, min.cells = 0, min.genes = 0, is.expr = 0, do.normalize = FALSE, do.scale = FALSE, do.center = FALSE, names.field = 1,names.delim = "_", add.cell.id1 = NULL, add.cell.id2 = 'KO2')
+EXP = MergeSeurat(EXP, KO294_3, project = NULL, min.cells = 0, min.genes = 0, is.expr = 0, do.normalize = FALSE, do.scale = FALSE, do.center = FALSE, names.field = 1,names.delim = "_", add.cell.id1 = NULL, add.cell.id2 = 'KO3')
+EXP = MergeSeurat(EXP, KO294_4, project = NULL, min.cells = 0, min.genes = 0, is.expr = 0, do.normalize = FALSE, do.scale = FALSE, do.center = FALSE, names.field = 1,names.delim = "_", add.cell.id1 = NULL, add.cell.id2 = 'KO4')
 
