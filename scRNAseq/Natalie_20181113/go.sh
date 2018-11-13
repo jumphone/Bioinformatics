b7a5b261debcf62b69837a9e3928e458a79d0dc7
Zhang, Feng <jumphone@163.com>
2018-11-13 13:13:20 -0500

    Add files via upload

---
+++ scRNAseq/Natalie_20181113/go.sh
@@ -0,0 +1,4 @@
+python changeName.py Natalie_4-8_out_gene_exon_tagged.dge.txt WT4to8
+python changeName.py Natalie_6wk_out_gene_exon_tagged.dge.txt WT6
+python combine.py EXP123456.combined.txt Natalie_4-8_out_gene_exon_tagged.dge.txt.named.txt EXP1234567.combined.txt
+python combine.py EXP1234567.combined.txt Natalie_6wk_out_gene_exon_tagged.dge.txt.named.txt EXP12345678.combined.txt
