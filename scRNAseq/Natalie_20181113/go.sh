0bd207422c4c9c755337c025935545f3bc3e2e68
Zhang, Feng <jumphone@163.com>
2018-11-13 14:05:04 -0500

    Update go.sh

--- scRNAseq/Natalie_20181113/go.sh
+++ scRNAseq/Natalie_20181113/go.sh
@@ -1,4 +1,5 @@
 python changeName.py Natalie_4-8_out_gene_exon_tagged.dge.txt WT4to8
 python changeName.py Natalie_6wk_out_gene_exon_tagged.dge.txt WT6
-python combine.py EXP123456.combined.txt Natalie_4-8_out_gene_exon_tagged.dge.txt.named.txt EXP1234567.combined.txt
-python combine.py EXP1234567.combined.txt Natalie_6wk_out_gene_exon_tagged.dge.txt.named.txt EXP12345678.combined.txt
+#python combine.py EXP123456.combined.txt Natalie_4-8_out_gene_exon_tagged.dge.txt.named.txt EXP1234567.combined.txt
+#python combine.py EXP1234567.combined.txt Natalie_6wk_out_gene_exon_tagged.dge.txt.named.txt EXP12345678.combined.txt
+python combine.py Natalie_4-8_out_gene_exon_tagged.dge.txt.named.txt Natalie_6wk_out_gene_exon_tagged.dge.txt.named.txt WT.txt
