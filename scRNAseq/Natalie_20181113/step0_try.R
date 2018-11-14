8db993e1d5610461359035cf6aa7da349809a0a9
Zhang, Feng <jumphone@163.com>
2018-11-14 11:22:29 -0500

    Update step0_try.R

--- scRNAseq/Natalie_20181113/step0_try.R
+++ scRNAseq/Natalie_20181113/step0_try.R
@@ -3,16 +3,16 @@ library(dplyr)
 
 pbmc.data <- read.table('ALL.txt',sep='\t',check.name=F,row.names=1,header=T)
 pbmc <- CreateSeuratObject(raw.data = pbmc.data, min.cells = 3, min.genes = 200, project = "Natalie")
+#save(pbmc,file='ALL_raw.RObj')
+
 mito.genes <- grep(pattern = "^mt-", x = rownames(x = pbmc@data), value = TRUE)
 percent.mito <- Matrix::colSums(pbmc@raw.data[mito.genes, ])/Matrix::colSums(pbmc@raw.data)
 pbmc <- AddMetaData(object = pbmc, metadata = percent.mito, col.name = "percent.mito")
 pbmc <- AddMetaData(object = pbmc, metadata = pbmc@ident, col.name = "batch")
 
 VlnPlot(object = pbmc, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
-pbmc=FilterCells(object = pbmc, subset.names = c("nGene", "percent.mito"), low.thresholds = c(500, -Inf), high.thresholds = c(2500, 0.1))
+pbmc=FilterCells(object = pbmc, subset.names = c("nGene", "percent.mito"), low.thresholds = c(200, -Inf), high.thresholds = c(2500, 0.1))
 pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
-
-#save(pbmc,file='ALL_raw.RObj')
 
 pbmc <- FindVariableGenes(object = pbmc, do.plot = F, mean.function = ExpMean, dispersion.function = LogVMR, x.low.cutoff =0, y.cutoff = 0.5)
 length(x=pbmc@var.genes) #3787
@@ -23,23 +23,22 @@ stim[which(pbmc@meta.data$batch=='WT4to8')]='wt'
 stim[which(pbmc@meta.data$batch=='WT6')]='wt'
 pbmc@meta.data$stim=stim
 
-PCNUM=40
-pbmc <- RunPCA(object = pbmc, pc.genes = pbmc@var.genes, do.print = TRUE, pcs.compute=PCNUM, pcs.print = 1:5,  genes.print = 5)
+NUM=40
+pbmc <- RunCCA(object = pbmc, pc.genes = pbmc@var.genes, do.print = TRUE, num.cc = NUM)
 
-DimPlot(object = pbmc, reduction.use = "pca", group.by = "stim",  pt.size = 0.5, do.return = F)
+DimPlot(object = pbmc, reduction.use = "cca", group.by = "stim",  pt.size = 0.5, do.return = F)
 
 
 DIM=1:35
-pbmc <- AlignSubspace(pbmc , reduction.type = "pca", grouping.var = "stim",  dims.align = DIM)
-DimPlot(object = pbmc, reduction.use = "pca.aligned", group.by = "stim",  pt.size = 0.5, do.return = F)
+pbmc <- AlignSubspace(pbmc , reduction.type = "cca", grouping.var = "stim",  dims.align = DIM)
+DimPlot(object = pbmc, reduction.use = "cca.aligned", group.by = "stim",  pt.size = 0.5, do.return = F)
 
 TSNE_DIM=1:35
-pbmc  <- RunTSNE(pbmc , reduction.use = "pca.aligned", dims.use = TSNE_DIM, do.fast = T)
+pbmc  <- RunTSNE(pbmc , reduction.use = "cca.aligned", dims.use = TSNE_DIM, do.fast = T)
 
 #save(pbmc,file='ALL_tsne.RObj')
 
-
-
+TSNEPlot(object = pbmc, group.by = "stim")
 
 
 
