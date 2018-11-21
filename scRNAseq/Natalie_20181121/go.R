29c9ebb1aad229694801563383e3cec4b9f2835f
Zhang, Feng <jumphone@163.com>
2018-11-21 12:16:18 -0500

    Update go.R

--- scRNAseq/Natalie_20181121/go.R
+++ scRNAseq/Natalie_20181121/go.R
@@ -1,1 +1,6 @@
+library('Seurat')
+GBM.data=read.table('./GBM/GBM_out_gene_exon_tagged.dge.txt',header=T,row.names=1)
+medullo.data=read.table('./medullo/medullo_out_gene_exon_tagged.dge.txt',header=T,row.names=1)
+nb6.data=read.table('./nb6/nb6_out_gene_exon_tagged.dge.txt',header=T,row.names=1)
+
 
