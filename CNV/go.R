8ac0ca1ad18c80d85427b9d7ad19822f23618d80
Zhang, Feng <jumphone@163.com>
2018-12-09 13:05:56 -0500

    Update go.R

--- CNV/go.R
+++ CNV/go.R
@@ -7,7 +7,7 @@ circos.initializeWithIdeogram(species='mm10')
 UPLIMIT=8
 LWLIMIT=-2
 CEX=0.3
-BCOL='grey50'
+BCOL='grey30'
 H=2
 ##############
 bed = read.table('NSC3.BedGraph.bed',sep='\t')
@@ -16,15 +16,16 @@ colnames(bed)=c('chr','start','end','value1','value2')
 
 circos.genomicTrackPlotRegion(bed, ylim = c(LWLIMIT, UPLIMIT), panel.fun = function(region, value, ...) {
     #print(value[,2])
-    
+   h=H
+   cell.xlim = get.cell.meta.data("cell.xlim")
+   circos.lines(cell.xlim, c(h, h), col = BCOL)
+        
    col = rep('grey50',length(value[,2]))
    col[which(value[,2]==1)]='red'
    col[which(value[,2]==-1)]='blue'
     #circos.genomicPoints(region, value, col = col, cex = 0.5, pch = 16)
    circos.genomicPoints(region, value[,1],col = col, cex = CEX, pch = 16)
-   h=H
-   cell.xlim = get.cell.meta.data("cell.xlim")
-   circos.lines(cell.xlim, c(h, h), col = BCOL)
+
     
 }, track.height = 0.1)
 
@@ -36,16 +37,15 @@ colnames(bed)=c('chr','start','end','value1','value2')
 
 circos.genomicTrackPlotRegion(bed, ylim = c(LWLIMIT, UPLIMIT), panel.fun = function(region, value, ...) {
     #print(value[,2])
+   h=H
+   cell.xlim = get.cell.meta.data("cell.xlim")
+   circos.lines(cell.xlim, c(h, h), col = BCOL)
     
    col = rep('grey50',length(value[,2]))
    col[which(value[,2]==1)]='red'
    col[which(value[,2]==-1)]='blue'
     #circos.genomicPoints(region, value, col = col, cex = 0.5, pch = 16)
-   circos.genomicPoints(region, value[,1],col = col, cex = CEX, pch = 16)
-     h=H
-   cell.xlim = get.cell.meta.data("cell.xlim")
-   circos.lines(cell.xlim, c(h, h), col = BCOL)
-    
+   circos.genomicPoints(region, value[,1],col = col, cex = CEX, pch = 16)  
 }, track.height = 0.1)
 
 
@@ -56,15 +56,16 @@ colnames(bed)=c('chr','start','end','value1','value2')
 
 circos.genomicTrackPlotRegion(bed, ylim = c(LWLIMIT, UPLIMIT), panel.fun = function(region, value, ...) {
     #print(value[,2])
+   h=H
+   cell.xlim = get.cell.meta.data("cell.xlim")
+   circos.lines(cell.xlim, c(h, h), col = BCOL)
     
    col = rep('grey50',length(value[,2]))
    col[which(value[,2]==1)]='red'
    col[which(value[,2]==-1)]='blue'
     #circos.genomicPoints(region, value, col = col, cex = 0.5, pch = 16)
    circos.genomicPoints(region, value[,1],col = col, cex = CEX, pch = 16)
-     h=H
-   cell.xlim = get.cell.meta.data("cell.xlim")
-   circos.lines(cell.xlim, c(h, h), col = BCOL)
+
     
 }, track.height = 0.1)
 
