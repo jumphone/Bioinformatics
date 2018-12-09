64da1648b0531b91e03ad734168b8945b8149a2f
Zhang, Feng <jumphone@163.com>
2018-12-09 12:17:25 -0500

    Update go.R

--- CNV/go.R
+++ CNV/go.R
@@ -1,5 +1,55 @@
 
 library(circlize)
 
+circos.initializeWithIdeogram(species='mm10')
 
+##############
+bed = read.table('NSC3.BedGraph.bed',sep='\t')
+bed[,4][which(bed1[,4]>4)]=4
+colnames(bed)=c('chr','start','end','value1','value2')
+
+circos.genomicTrackPlotRegion(bed, ylim = c(-4, 4), panel.fun = function(region, value, ...) {
+    #print(value[,2])
+    
+   col = rep('grey50',length(value[,2]))
+   col[which(value[,2]==1)]='red'
+   col[which(value[,2]==-1)]='blue'
+    #circos.genomicPoints(region, value, col = col, cex = 0.5, pch = 16)
+   circos.genomicPoints(region, value[,1],col = col, cex = 0.1, pch = 16)
+    
+}, track.height = 0.1)
+
+
+##############
+bed = read.table('NSC4.BedGraph.bed',sep='\t')
+bed[,4][which(bed1[,4]>4)]=4
+colnames(bed)=c('chr','start','end','value1','value2')
+
+circos.genomicTrackPlotRegion(bed, ylim = c(-4, 4), panel.fun = function(region, value, ...) {
+    #print(value[,2])
+    
+   col = rep('grey50',length(value[,2]))
+   col[which(value[,2]==1)]='red'
+   col[which(value[,2]==-1)]='blue'
+    #circos.genomicPoints(region, value, col = col, cex = 0.5, pch = 16)
+   circos.genomicPoints(region, value[,1],col = col, cex = 0.1, pch = 16)
+    
+}, track.height = 0.1)
+
+
+##############
+bed = read.table('NSC7.BedGraph.bed',sep='\t')
+bed[,4][which(bed1[,4]>4)]=4
+colnames(bed)=c('chr','start','end','value1','value2')
+
+circos.genomicTrackPlotRegion(bed, ylim = c(-4, 4), panel.fun = function(region, value, ...) {
+    #print(value[,2])
+    
+   col = rep('grey50',length(value[,2]))
+   col[which(value[,2]==1)]='red'
+   col[which(value[,2]==-1)]='blue'
+    #circos.genomicPoints(region, value, col = col, cex = 0.5, pch = 16)
+   circos.genomicPoints(region, value[,1],col = col, cex = 0.1, pch = 16)
+    
+}, track.height = 0.1)
 
