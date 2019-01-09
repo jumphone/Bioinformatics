1f25996633e089a7d6abf5709064e779db5ec554
Zhang, Feng <jumphone@163.com>
2019-01-09 17:00:58 -0500

    Update 1CompareATAC_GBM_LGG.R

--- GBM/1CompareATAC_GBM_LGG.R
+++ GBM/1CompareATAC_GBM_LGG.R
@@ -88,8 +88,8 @@ PC1_PEAK_LOADING=pbmc@dr$pca@gene.loadings[,1]
 PC2_PEAK_LOADING=pbmc@dr$pca@gene.loadings[,2]
 PC3_PEAK_LOADING=pbmc@dr$pca@gene.loadings[,3]
 PC4_PEAK_LOADING=pbmc@dr$pca@gene.loadings[,4]
-HC= 0.002
-LC= -0.002
+HC= 0.005
+LC= -0.005
 
 
 par(mfrow=c(2,2))
@@ -102,8 +102,11 @@ plot(density(PC2_PEAK_LOADING))
 plot(density(PC3_PEAK_LOADING))
 plot(density(PC4_PEAK_LOADING))
 
+PC1_GENE=PC1_PEAK_LOADING
 PC1_POS=PC1_GENE[which(PC1_GENE >HC)]
 PC1_NEG=PC1_GENE[which(PC1_GENE <LC)]
+length(PC1_POS)
+length(PC1_NEG)
 
 PC1_POS_N=names(PC1_POS)
 
@@ -113,8 +116,8 @@ for(one in tmp){
     PC1_POS_N_S = cbind(PC1_POS_N_S, one)
            }
 PC1_POS_OUT=cbind(t(PC1_POS_N_S),PC1_POS)
-
-
+colnames(PC1_POS_OUT)=c('CHR','START','END','TYPE','ID','PEAK_SCORE','REGION','GC','PC1_LOADING')
+write.table(PC1_POS_OUT,'PC1_POS_OUT.txt',row.names=F,col.names=T,sep='\t',quote=F)
 
 PC1_NEG_N=names(PC1_NEG)
 tmp=strsplit(PC1_NEG_N, "_")
@@ -124,8 +127,47 @@ for(one in tmp){
            }
 PC1_NEG_OUT=cbind(t(PC1_NEG_N_S),PC1_NEG)
 
+colnames(PC1_NEG_OUT)=c('CHR','START','END','TYPE','ID','PEAK_SCORE','REGION','GC','PC1_LOADING')
+write.table(PC1_NEG_OUT,'PC1_NEG_OUT.txt',row.names=F,col.names=T,sep='\t',quote=F)
 
 
+################################################################################################
+
+PC1_POS_OUT=read.delim('PC1_POS_OUT.txt',sep='\t',header=T)
+PC1_NEG_OUT=read.delim('PC1_NEG_OUT.txt',sep='\t',header=T)
 
 
+rawbed = PC1_POS_OUT 
+pc1_loading_scale=as.numeric(rawbed[,9])
+bed=rawbed[,1:4]
+colnames(bed)=c('chr','start','end','value')
+bed=as.data.frame(bed)
+bed$chr=as.character(rawbed[,1])
+bed$start=as.numeric(rawbed[,2])
+bed$end=as.numeric(rawbed[,3])
+bed$value=pc1_loading_scale
+bed=bed[order(bed[,2]),]
+bed=bed[order(bed[,1]),]
+
+UPLIMIT=2
+LWLIMIT=-2
+CEX=0.3
+BCOL='grey30'
+H=2
+
+library(circlize)
+circos.initializeWithIdeogram(species='hg38', chromosome.index = paste0("chr", c(1:22, "X", "Y")))
+circos.genomicTrackPlotRegion(bed, ylim = c(LWLIMIT, UPLIMIT), panel.fun = function(region, value, ...) {
+    #print(value[,2])
+   h=H
+   cell.xlim = get.cell.meta.data("cell.xlim")
+   #circos.lines(cell.xlim, c(h, h), col = BCOL)
+        
+   COL = rep('grey50',length(value[,1]))
+   COL[which(value[,1]>0)]='red'
+   COL[which(value[,1]<0)]='blue'
+   circos.genomicPoints(region,type='h',col=COL, value[,1], cex = CEX, pch = 16)
+   #print(region)
+   #circos.rect(region[1], 0, region[2], 0.5, col = 'red')
+}, track.height = 0.1)
 
