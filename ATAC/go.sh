f33bbdfd447ea9e03e72e8d9cc35838e19a5f7dc
Zhang, Feng <jumphone@163.com>
2019-01-11 10:54:18 -0500

    Update go.sh

--- ATAC/go.sh
+++ ATAC/go.sh
@@ -3,7 +3,7 @@ WT=
 
 /usr/bin/macs2 callpeak -t  $WT $KO -n ./POOL -B -f BAM --shift -75 --extsize 150 --nomodel --call-summits --nolambda --keep-dup all -p 0.01 -g mm
 
-pythonks.* > ALL_PEAK.bed
+cat *_peaks.narrowPeak > ALL_PEAK.bed
 bedtools sort -i ALL_PEAK.bed > ALL_PEAK.sorted.bed
 bedtools merge -i ALL_PEAK.sorted.bed > ALL_PEAK.sorted.merged.bed
 samtools stats $KO > $KO\.stats
