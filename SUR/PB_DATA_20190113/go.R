4907e5e9104e96328571b142310661eb85c84842
Zhang, Feng <jumphone@163.com>
2019-01-14 16:21:45 -0500

    Update go.R

--- SUR/PB_DATA_20190113/go.R
+++ SUR/PB_DATA_20190113/go.R
@@ -29,3 +29,27 @@ surv_pvalue(fit)
 
 
 ####################################
+
+mut_data=read.table('COM.txt',header=T,sep='\t',row.names=1)
+library(Seurat)
+pbmc <- CreateSeuratObject(raw.data = mut_data, min.cells = 0, min.genes = 0, project = "PBTR")
+pbmc@meta.data$nMut=pbmc@meta.data$nGene
+VlnPlot(object = pbmc, features.plot = c("nMut"), nCol = 1)
+dim(pbmc@data)
+pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", 
+    scale.factor = 10000)
+pbmc <- FindVariableGenes(object = pbmc,do.plot=F, mean.function = ExpMean, dispersion.function = LogVMR, 
+    x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
+length(x = pbmc@var.genes)
+#50,156
+pbmc <- ScaleData(object = pbmc)
+PCNUM=20
+pbmc <- RunPCA(object = pbmc, pcs.compute=PCNUM,pc.genes = pbmc@var.genes, do.print = TRUE, pcs.print = 1:5, 
+    genes.print = 5)
+PCElbowPlot(object = pbmc)
+pbmc <- RunTSNE(object = pbmc, dims.use = 1:7, do.fast = TRUE,perplexity=5)
+TSNEPlot(object = pbmc,pt.size=3)
+
+
+
+
