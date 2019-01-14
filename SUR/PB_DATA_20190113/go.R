3d851db1a6d43d006811d6db5025291a0d0e1a2d
Zhang, Feng <jumphone@163.com>
2019-01-13 20:45:18 -0500

    Create go.R

---
+++ SUR/PB_DATA_20190113/go.R
@@ -0,0 +1,22 @@
+SUR=read.table('SUR.txt',sep='\t',header=T)
+used=which(!is.na(SUR[,2]))
+
+library(survival)
+library(survminer)
+
+score=SUR[used,2]
+this_sur=SUR[used,3]
+highcut=0
+lowcut=0
+
+TYPE=rep('MED',length(used))
+TYPE[which(score> highcut)]='High'
+TYPE[which(score< lowcut)]='Low'
+surtime=this_sur[which(TYPE!='MED')]
+surevent=rep(1,length(which(TYPE!='MED')))
+surtype=TYPE[which(TYPE!='MED')]
+surtype=as.data.frame(surtype)
+surv_object <- Surv(time = surtime, event = surevent)
+fit <- survfit(surv_object ~ surtype, data=surtype)
+ggsurvplot(fit, pval = TRUE)
+surv_pvalue(fit)
