4e53a68bf01f4ec6a069087fe4d70d5470956705
Zhang, Feng <jumphone@163.com>
2019-01-15 14:15:48 -0500

    Update go.R

--- scRNAseq/LIGUO_20190115/go.R
+++ scRNAseq/LIGUO_20190115/go.R
@@ -6,7 +6,7 @@ T=.simple_combine(cbind(T1,T1),cbind(T2,T2))$combine
 T=T[,c(1,3)]
 N=read.table('Reference_expression.txt',sep='\t',row.names=1,header=TRUE)
 REF=.simple_combine(T,N)$combine
-
+REF=REF[,c(1,3,4,5,6,7,8,9)]
 #######################################################
 
 load('10xEXP.Robj')
@@ -22,14 +22,14 @@ exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
 exp_ref_mat=REF 
 out=.get_cor(exp_sc_mat, exp_ref_mat, method='spearman',CPU=4, print_step=10)
 tag=.get_tag_max(out)
-write.table(tag,file='10X_spearman.txt',quote=F,row.names=F,col.names=T,sep='\t')
+write.table(tag,file='10X_spearman.txt', quote=F,row.names=F,col.names=TRUE,sep='\t')
 pbmc@meta.data$tag=tag[,2]
 pdf('10X.pdf',width=7,height=5)
 TSNEPlot(pbmc,group.by='tag')
 dev.off()
 
 #######################################################
-load('DropseqEXP.Robj')
+load('dropEXP.Robj')
 pbmc=EXP
 COL=c()
 i=1
@@ -42,7 +42,7 @@ exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
 exp_ref_mat=REF 
 out=.get_cor(exp_sc_mat, exp_ref_mat, method='spearman',CPU=4, print_step=10)
 tag=.get_tag_max(out)
-write.table(tag,file='Dropseq_spearman.txt',quote=F,row.names=F,col.names=T,sep='\t')
+write.table(tag,file='Dropseq_spearman.txt',quote=F,row.names=F,col.names=TRUE,sep='\t')
 pbmc@meta.data$tag=tag[,2]
 pdf('Dropseq.pdf',width=7,height=5)
 TSNEPlot(pbmc,group.by='tag')
