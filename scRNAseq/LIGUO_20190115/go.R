f96304cbf26101774996706cfd9503f23282a564
Zhang, Feng <jumphone@163.com>
2019-01-15 11:37:24 -0500

    Update go.R

--- scRNAseq/LIGUO_20190115/go.R
+++ scRNAseq/LIGUO_20190115/go.R
@@ -1,1 +1,14 @@
+source('scRef.R')
+
+T1=read.table('10_P19_cKO1.txt.uniq.txt',sep='\t',row.names=1,header=T)
+T2=read.table('11_P19_cKO2.txt.uniq.txt',sep='\t',row.names=1,header=T)
+T=.simple_combine(cbind(T1,T1),cbind(T2,T2))$combine
+T=T[,c(1,3)]
+N=read.table('Reference_expression.txt',sep='\t',row.names=1,header=TRUE)
+REF=.simple_combine(T,N)$combine
+
+
+
+
+
 
