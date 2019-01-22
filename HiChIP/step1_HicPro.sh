ce309de9f823f420c8db76d08bc961d3f26a291a
Zhang, Feng <jumphone@163.com>
2019-01-22 12:39:46 -0500

    Update step1_HicPro.sh

--- HiChIP/step1_HicPro.sh
+++ HiChIP/step1_HicPro.sh
@@ -1,3 +1,12 @@
 #https://github.com/nservant/HiC-Pro
 
 
+#/home/disk/zhangfeng/project/HiChip/download/HiC-Pro-bin/HiC-Pro_2.11.1/bin/utils/digest_genome.py -r mboi -o MBOI_resfrag_rn5.bed rn5.fa
+
+HicPro=/home/disk/zhangfeng/project/HiChip/download/HiC-Pro-bin/HiC-Pro_2.11.1/bin/HiC-Pro
+INPUT=/home/disk/zhangfeng/project/HiChip/rawdata
+OUTPUT=/home/disk/zhangfeng/project/HiChip/output
+CONFIG=/home/disk/zhangfeng/project/HiChip/CONFIG1.txt
+
+$HicPro -i $INPUT -o $OUTPUT -c $CONFIG
+
