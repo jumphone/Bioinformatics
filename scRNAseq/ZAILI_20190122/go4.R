736971aebfcbaabe47a6528607629e7785e5c64d
Zhang, Feng <jumphone@163.com>
2019-01-24 11:34:34 -0500

    Create go4.R

---
+++ scRNAseq/ZAILI_20190122/go4.R
@@ -0,0 +1,21 @@
+library(Seurat)
+library(dplyr)
+
+pbmc.data <- readRDS('cerebullum_dev.RDS')
+pbmc <- CreateSeuratObject(raw.data = pbmc.data, min.cells = 0, min.genes = 0, project = "10X_PBMC")
+
+mito.genes <- grep(pattern = "^mt-", x = rownames(x = pbmc@data), value = TRUE)
+percent.mito <- Matrix::colSums(pbmc@raw.data[mito.genes, ])/Matrix::colSums(pbmc@raw.data)
+pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+
+pbmc <- FindVariableGenes(object = pbmc, mean.function = ExpMean, dispersion.function = LogVMR, 
+    x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
+      
+length(x = pbmc@var.genes)
+#4602
+
+pbmc <- ScaleData(object = pbmc, genes.use =pbmc@var.genes, vars.to.regress = c("nUMI", "percent.mito"),num.cores =4)
+
+
+
+
