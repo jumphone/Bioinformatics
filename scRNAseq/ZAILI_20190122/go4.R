5e397372478feb327f096f5ddddad4e51eb665ef
Zhang, Feng <jumphone@163.com>
2019-01-24 13:30:40 -0500

    Update go4.R

--- scRNAseq/ZAILI_20190122/go4.R
+++ scRNAseq/ZAILI_20190122/go4.R
@@ -33,36 +33,95 @@ pbmc@meta.data$clust=meta.data[,14]
 saveRDS(pbmc,'cb_seurat.RDS')
 ########################################
 
-
 pbmc=readRDS('cb_seurat.RDS')
 
-pdf('CellType.pdf',width=20,height=18)
+TAB=table(pbmc@meta.data$clust,pbmc@meta.data$tag)
+write.table(TAB,file='Sup1_CellTypeAndClust.txt',sep='\t',quote=F,row.names=T,col.names=T)
+
+pdf('Sup2_CellTypeAndClust.pdf',width=20,height=18)
 TSNEPlot(pbmc,group.by='tag',do.label=TRUE)
+TSNEPlot(pbmc,group.by='clust',do.label=TRUE)
 dev.off()
 
+source('scRef.R')
 ref_vec=pbmc@dr$tsne@cell.embeddings
-ref_tag=cbind(names(pbmc@ident), as.character(pbmc@meta.data$tag))    
+ref_tag=cbind(names(pbmc@ident), as.character(pbmc@meta.data$clust))    
 exp_ref_mat=as.matrix(pbmc@raw.data)
 LocalRef= .generate_ref(exp_ref_mat, ref_tag, min_cell = 10 )  
+write.table(LocalRef,'Sup3_ClustRef.txt',sep='\t',quote=F,row.names=T,col.names=T)
+
+saveRDS(exp_ref_mat,'./Sup4_projection/exp_ref_mat.RDS')
+saveRDS(ref_tag,'./Sup4_projection/ref_tag.RDS')
 
 ########################################
-scdata=readRDS('R4_umap.RDS')
-
-source('scRef.R')
-exp_sc_mat=as.matrix(scdata@raw.data)
-  
+########################################
+TAG='R4'
+################
+scdata=readRDS(paste0(TAG,'_umap.RDS'))
+exp_sc_mat=as.matrix(scdata@raw.data) 
 sc_tag=SCREF(exp_sc_mat, LocalRef)$tag2
-
+################
+this_tab=table(sc_tag[,2])
+this_lab=c()
+for(one in names(this_tab)){ 
+    tmp_lab=colnames(TAB)[which(TAB[which(rownames(TAB)==one),]>0)]
+    this_lab=c(this_lab, tmp_lab)
+}
+################
+this_stat=cbind(names(this_tab),this_lab,this_tab)
+colnames(this_stat)=c('cluster','cell_type','cell_num')
+write.table(this_stat,paste0('./Sup4_projection/',TAG,'_stat.txt'),sep='\t',quote=F,row.names=F,col.names=F)
+################
 out =.vec_projection(exp_sc_mat, sc_tag, exp_ref_mat, ref_tag, ref_vec, 
         method='kendall', nearest_cell=1, alpha=0.5, random_size=300, 
         random_seed=123, CPU=4, print_step=10)
-
+################
+saveRDS(exp_sc_mat,paste0('./Sup4_projection/',TAG,'_exp_sc_mat.RDS'))
+saveRDS(sc_tag,paste0('./Sup4_projection/',TAG,'_sc_tag.RDS'))
+saveRDS(out,paste0('./Sup4_projection/',TAG,'_out.RDS'))
+################
+pdf(paste0('./Sup4_projection/',TAG,'_proj.pdf'),width=7,height=7)
 XLIM=c(min(ref_vec[,1]-1),max(ref_vec[,1]+1))
 YLIM=c(min(ref_vec[,2]-1),max(ref_vec[,2]+1))
-
-plot(ref_vec,xlim=XLIM,ylim=YLIM,pch=16,col='grey70')
+plot(ref_vec,xlim=XLIM,ylim=YLIM,pch=16,col='grey70',cex=0.5)
 par(new=T)
-plot(out$vec,xlim=XLIM,ylim=YLIM,pch=16,col='red',)
+plot(out$vec,xlim=XLIM,ylim=YLIM,pch=16,col='red',cex=0.3)
+dev.off()
 
+########################################
+########################################
 
+TAG='R18058913'
+################
+scdata=readRDS(paste0(TAG,'_umap.RDS'))
+exp_sc_mat=as.matrix(scdata@raw.data) 
+sc_tag=SCREF(exp_sc_mat, LocalRef)$tag2
+################
+this_tab=table(sc_tag[,2])
+this_lab=c()
+for(one in names(this_tab)){ 
+    tmp_lab=colnames(TAB)[which(TAB[which(rownames(TAB)==one),]>0)]
+    this_lab=c(this_lab, tmp_lab)
+}
+################
+this_stat=cbind(names(this_tab),this_lab,this_tab)
+colnames(this_stat)=c('cluster','cell_type','cell_num')
+write.table(this_stat,paste0('./Sup4_projection/',TAG,'_stat.txt'),sep='\t',quote=F,row.names=F,col.names=F)
+################
+out =.vec_projection(exp_sc_mat, sc_tag, exp_ref_mat, ref_tag, ref_vec, 
+        method='kendall', nearest_cell=1, alpha=0.5, random_size=300, 
+        random_seed=123, CPU=4, print_step=10)
+################
+saveRDS(exp_sc_mat,paste0('./Sup4_projection/',TAG,'_exp_sc_mat.RDS'))
+saveRDS(sc_tag,paste0('./Sup4_projection/',TAG,'_sc_tag.RDS'))
+saveRDS(out,paste0('./Sup4_projection/',TAG,'_out.RDS'))
+################
+pdf(paste0('./Sup4_projection/',TAG,'_proj.pdf'),width=7,height=7)
+XLIM=c(min(ref_vec[,1]-1),max(ref_vec[,1]+1))
+YLIM=c(min(ref_vec[,2]-1),max(ref_vec[,2]+1))
+plot(ref_vec,xlim=XLIM,ylim=YLIM,pch=16,col='grey70',cex=0.5)
+par(new=T)
+plot(out$vec,xlim=XLIM,ylim=YLIM,pch=16,col='red',cex=0.3)
+dev.off()
 
+########################################
