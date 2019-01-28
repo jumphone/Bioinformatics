8828b0ed9a727db7cf4c6be6cb2071ce19718302
Zhang, Feng <jumphone@163.com>
2019-01-27 19:02:47 -0500

    Update 1CompareATAC_GBM_LGG.R

--- GBM/1CompareATAC_GBM_LGG.R
+++ GBM/1CompareATAC_GBM_LGG.R
@@ -1,3 +1,6 @@
+#/home/zhangfeng/disk/project/ATAC/data
+
+
 TAG=as.character(read.table('GBMLGG_deNeg_ATAC_PanCan_Log2Norm_Counts.TAG.txt',header=F)[,1])
 #a=read.table('GBMLGG_deNeg_ATAC_PanCan_Log2Norm_Counts.txt',row.names=1,header=T,sep='\t',check.names = F)
 a=readRDS('GBMLGG_deNeg_ATAC_PanCan_Log2Norm_Counts.RDS')
