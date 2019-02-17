909d3d341cb59f7db696bf4e662cb24d7f09f830
Zhang, Feng <jumphone@163.com>
2019-02-16 19:58:04 -0500

    Create go.R

---
+++ scRNAseq/Hong_20190216/go.R
@@ -0,0 +1,8 @@
+library(Seurat)
+library(dplyr)
+
+
+#data_6678 <- read.delim('E-MTAB-6678.processed.2/SS2_raw_counts.csv',header=T,row.names=1,sep=',')
+#saveRDS(data_6678,file='data_6678.RDS')
+#data_6701 <- read.delim('E-MTAB-6701.processed.1/decidua_data_10x.txt',header=T,row.names=1)
+
