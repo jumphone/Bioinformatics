782cf39d753e5839e045f76ec61a2c48039746ac
Zhang, Feng <jumphone@163.com>
2019-02-16 22:09:25 -0500

    Update go.R

--- scRNAseq/Hong_20190216/go.R
+++ scRNAseq/Hong_20190216/go.R
@@ -14,13 +14,16 @@ colnames(data_6701)=paste0('cluster',colnames(data_6701))
 data_6701[1:3,1:3]
 
 source('scRef.R')
+n6678=.check_pos(data_6678)
+n6701=.check_pos(data_6701)
 
-out=.get_cor(data_6678, data_6701, method='kendall',CPU=4, print_step=10)
+data_6678_trim=.trim_pos(data_6678, min(n6678))  
+data_6701_trim=.trim_pos(data_6701, min(n6701))  
 
-
-
-
-
+exp_sc_mat=data_6678_trim
+exp_ref_mat=data_6701_trim
+out=.get_cor(exp_sc_mat, exp_ref_mat, method='kendall',CPU=4, print_step=10)
+tag=.get_tag_max(out)
 
 
 
