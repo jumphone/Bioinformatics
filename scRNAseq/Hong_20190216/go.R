a8e9e4733bc904fcd1999211ac09ca4fa4517852
Zhang, Feng <jumphone@163.com>
2019-02-16 21:55:51 -0500

    Update go.R

--- scRNAseq/Hong_20190216/go.R
+++ scRNAseq/Hong_20190216/go.R
@@ -2,7 +2,27 @@ library(Seurat)
 library(dplyr)
 
 
-#data_6678 <- read.delim('E-MTAB-6678.processed.2/SS2_raw_counts.csv',header=T,row.names=1,sep=',')
-#saveRDS(data_6678,file='data_6678.RDS')
-#data_6701 <- read.delim('E-MTAB-6701.processed.1/decidua_data_10x.txt',header=T,row.names=1)
+data_6678 <- read.table('ref_6678_pure.txt',header=T,row.names=1,sep='\t',check.names=F)
+data_6678[1:3,1:3]
+colnames(data_6678)=paste0('cluster',colnames(data_6678))
+data_6678[1:3,1:3]
+
+
+data_6701 <- read.table('ref_6701_pure.txt',header=T,row.names=1,sep='\t',check.names=F)
+data_6701[1:3,1:3]
+colnames(data_6701)=paste0('cluster',colnames(data_6701))
+data_6701[1:3,1:3]
+
+source('scRef.R')
+
+out=.get_cor(data_6678, data_6701, method='kendall',CPU=4, print_step=10)
+
+
+
+
+
+
+
+
+
 
