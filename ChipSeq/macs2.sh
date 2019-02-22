3e32068e4a1f645c5e80dcdec39b659a91108345
Zhang, Feng <jumphone@163.com>
2019-02-22 15:32:00 -0500

    Update macs2.sh

--- ChipSeq/macs2.sh
+++ ChipSeq/macs2.sh
@@ -1,4 +1,4 @@
 bam=$1
 species=$2 #mm,hs
-macs2 callpeak -t $bam\.dedup.sorted.bam -f BAM -g $species -n $bam\.dedup.sorted.bam.macs2_narrow -B -q 0.01
-macs2 callpeak -t $bam\.dedup.sorted.bam  --broad -g $species -n $bam\.dedup.sorted.bam.macs2_broad --broad-cutoff 0.1
+macs2 callpeak -t $bam -f BAM -g $species -n $bam\.macs2_narrow -B -q 0.01
+macs2 callpeak -t $bam  --broad -g $species -n $bam\.macs2_broad --broad-cutoff 0.1
