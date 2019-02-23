ef8c35aa56ca0f5288bb38ff70b1364cae5c79ae
Zhang, Feng <jumphone@163.com>
2019-02-23 16:11:39 -0500

    Update go.R

--- STAT/go.R
+++ STAT/go.R
@@ -1,6 +1,27 @@
 a=read.table('data.txt',header=T,row.names=1,sep='\t')
+b=a[which(!is.na(a[,1]*a[,2])),]
+tmp=t.test(b[,2],b[,1],paired=T)
+true_stat=tmp$statistic
 
 c=c(a[,1],a[,2])
+c=c[which(!is.na(c))]
+
+END=100000
+stat_list=c()
+
+set.seed(123)
+i=1
+while(i<=END){
+  d1=sample(c,5)
+  d2=sample(c,5) 
+  this_stat=t.test(d2,d1,paired=T)$statistic
+  stat_list=c(stat_list,this_stat)  
+print(i)
+i=i+1}
+
+
+
+
 
 
 
