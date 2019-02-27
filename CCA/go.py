015485ac86ac5ac4b62f2ef8a25cbc995abf4fac
Zhang, Feng <jumphone@163.com>
2019-02-27 16:15:37 -0500

    Update go.py

--- CCA/go.py
+++ CCA/go.py
@@ -46,16 +46,54 @@ exp=np.transpose(exp)
 np.shape(exp)
 #np.save('exp', exp)
 #########################
-MSexp=exp[:,MSI]
-CTexp=exp[:,CTI]
+########################
+
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from MulticoreTSNE import MulticoreTSNE as TSNE
+from matplotlib import pyplot as plt
+import numpy as np
+
+NT=8
+RS=123
+PP=30
+NC=1
+PCP=0.95
+exp=np.load('exp.npy')
+###########
+
+fi=open('GSE118257_MSCtr_snRNA_FinalAnnotationTable.txt')
+header=fi.readline()
+anno=[]
+for line in fi:
+    seq=line.rstrip().split('\t')
+    anno.append(seq[3])
+fi.close()
+
+MSI=list(np.where(np.array(anno)=='MS')[0])
+CTI=list(np.where(np.array(anno)=='Ctrl')[0])
+#################
+
+MSexp=exp[MSI,:]
+CTexp=exp[CTI,:]
 np.shape(MSexp)
 np.shape(CTexp)
 
 #####################
-MS_pca=PCA(n_components=PCP).fit_transform(MSexp)
+from sklearn import preprocessing
+
+tmp=np.apply_along_axis(np.var, 0, MSexp)
+used=np.where(tmp>0)[0]
+MSexp=MSexp[:,used]
+np.shape(MSexp)
+
+pt = preprocessing.PowerTransformer(method='box-cox', standardize=True)
+PT_MSexp=pt.fit_transform((MSexp+1))
+
+PCA_PT_MSexp=PCA(n_components=PCP).fit_transform(PT_MSexp)
 
 
-MS_embeddings = TSNE(n_jobs=NT,n_components=NC,perplexity=PP,random_state=RS).fit_transform(MSexp)
+MS_embeddings = TSNE(n_jobs=NT,n_components=NC,perplexity=PP,random_state=RS).fit_transform(PCA_PT_MSexp)
 MS_x = MS_embeddings[:, 0]
 
 CT_embeddings = TSNE(n_jobs=NT,n_components=NC,perplexity=PP,random_state=RS).fit_transform(CTexp)
