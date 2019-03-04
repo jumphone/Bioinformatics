46f648841c7a286cfeead31e687498db44b9464f
Zhang, Feng <jumphone@163.com>
2019-03-04 15:23:01 -0500

    Create go1.R

---
+++ CCA/go1.R
@@ -0,0 +1,11 @@
+source('https://raw.githubusercontent.com/jumphone/Bioinformatics/master/CCA/scPA.R')
+source('https://raw.githubusercontent.com/jumphone/scRef/master/scRef.R')
+
+MS=readRDS('MS.RDS')
+CT=readRDS('CT.RDS')
+
+MSD=.data2one(MS)
+CTD=.data2one(CT)
+
+
+
