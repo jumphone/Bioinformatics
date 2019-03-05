7b0cc2961df36bdcb261a83ad367bf2dee1ca581
Zhang, Feng <jumphone@163.com>
2019-03-05 16:37:17 -0500

    Create go4.R

---
+++ CCA/go4.R
@@ -0,0 +1,29 @@
+
+
+source('https://raw.githubusercontent.com/jumphone/Bioinformatics/master/CCA/BAST.R')
+source('https://raw.githubusercontent.com/jumphone/scRef/master/scRef.R')
+
+D1=readRDS('CT.RDS')
+D2=readRDS('MS.RDS')
+
+D1=as.matrix(D1)
+D2=as.matrix(D2)
+
+library(scran)
+
+gene.counts1=D1
+sce1 <- SingleCellExperiment(list(counts=gene.counts1))
+sce1 <- normalize(sce1)
+
+gene.counts2=D2
+sce2 <- SingleCellExperiment(list(counts=gene.counts2))
+sce2 <- normalize(sce2)
+
+b1 <- sce1
+b2 <- sce2
+out <- fastMNN(b1, b2)
+
+
+bastout=BAST(CT,MS,FDR=0.05,COR=0.8)
+
+
