8e1cf2e7222b3fc9ec96cb443773b39f3f0f197a
Zhang, Feng <jumphone@163.com>
2019-03-05 21:36:24 -0500

    Update BAST.R

--- CCA/BAST.R
+++ CCA/BAST.R
@@ -97,46 +97,82 @@
   
     vindex1=which(GROUP %in% VP[,1])
     vindex2=which(GROUP %in% VP[,2])
+  
     
     print('Start')
     THIS_DR=1
     while(THIS_DR<=ncol(DR)){
         THIS_PC = DR[,THIS_DR]
-        M1=c()
-        M2=c()
-        maplst1=c()
-        maplst2=c()
+       
+        ########################
+        sd_lst1=c()
+        mean_lst1=c()
+        max_lst1=max(THIS_PC[vindex1])
+        min_lst1=min(THIS_PC[vindex1])
+        sd_lst2=c()
+        mean_lst2=c()  
+        max_lst2=max(THIS_PC[vindex2])
+        min_lst2=min(THIS_PC[vindex2])
         i=1
-        while(i<=nrow(VALID_PAIR)){
-            this_pair=VALID_PAIR[i,]
-            this_index1=which(GROUP %in% this_pair[1])
-            this_index2=which(GROUP %in% this_pair[2])
-            seq1=sort(THIS_PC[this_index1])
-            seq2=sort(THIS_PC[this_index2])
-            this_aln=dtw(seq1,seq2,keep=TRUE)
-            maplst1=c(maplst1, seq1[this_aln$index1])
-            maplst2=c(maplst2, seq2[this_aln$index2])
-            
+        while(i<=nrow(VP)){
+            p1=which(GROUP %in% VP[i,1])
+            p2=which(GROUP %in% VP[i,2])
+            sd1=sd(THIS_PC[p1])
+            mean1=mean(THIS_PC[p1])
+            sd2=sd(THIS_PC[p2])
+            mean2=mean(THIS_PC[p2])
+            sd_lst1=c(sd_lst1,sd1)
+            sd_lst2=c(sd_lst2,sd2)
+            mean_lst1=c(mean_lst1,mean1)
+            mean_lst2=c(mean_lst2,mean2)
             i=i+1}
-        comlst=cbind(maplst1,maplst2)
-        compc=apply(comlst,1,mean)
-        #compc=maplst2
-        
-        #plot(comlst[,1],compc)
-        comlst1o=order(comlst[,1])
-        #.findlst1 <-function(x){y=sample(compc[which(comlst[,1]==x)],1);return(y)}
-        .findlst1 <-function(x){y=mean(compc[which(comlst[,1]==x)]);return(y)}
-        vlst1lst1=  comlst[,1][comlst1o][match.closest(DR[index1,THIS_DR], comlst[,1][comlst1o])]
-        lst1lst1=apply(as.matrix(vlst1lst1),1,.findlst1) 
+    
+
+        mean_com= apply(cbind(mean_lst1,mean_lst2),1,mean)    
+   
+      
+        .x1_to_com=function(x1){
+            #if(x1 <=min_lst1){x1=min_lst1}
+            #if(x1 >=max_lst1){x1=max_lst1}
                 
-        comlst2o=order(comlst[,2])
-        #.findlst2 <-function(x){y=sample(compc[which(comlst[,2]==x)],1);return(y)}
-        .findlst2 <-function(x){y=mean(compc[which(comlst[,2]==x)]);return(y)}
-        vlst2lst2=  comlst[,2][comlst2o][match.closest(DR[index2,THIS_DR], comlst[,2][comlst2o])]
-        lst2lst2=apply(as.matrix(vlst2lst2),1,.findlst2)
-        #OO=order(THIS_PC[vindex2])
-        #lst2lst2 = THIS_PC[vindex2][OO][match.closest(DR[index2,THIS_DR], THIS_PC[vindex2][OO])]
-        #plot(lst2lst2,DR[index2,THIS_DR])
+            x1=x1
+            dlst1=c()
+            i=1
+            while(i<=nrow(VP)){
+                this_sd=sd_lst1[i]
+                this_mean=mean_lst1[i]
+                this_d=dnorm(x1,sd=this_sd,mean=this_mean)
+                if(is.na(this_d)){this_d=0}
+                dlst1=c(dlst1,this_d)
+                i=i+1} 
+                out=sum(dlst1/sum(dlst1)*mean_com)
+            return(out)}
+      
+        .x2_to_com=function(x2){
+            #if(x2 <=min_lst2){x2=min_lst2}
+            #if(x2 >=max_lst2){x2=max_lst2}
+                
+            x2=x2
+            dlst2=c()
+            i=1
+            while(i<=nrow(VP)){
+                this_sd=sd_lst2[i]
+                this_mean=mean_lst2[i]
+                this_d=dnorm(x2,sd=this_sd,mean=this_mean)
+                if(is.na(this_d)){this_d=0}
+                dlst2=c(dlst2,this_d)
+                i=i+1} 
+                out=sum(dlst2/sum(dlst2)*mean_com)
+            return(out)}
+         
+         ########################
+         #tmp=c(-10:20)
+         #tmp1=apply(as.matrix(tmp),1,.x1_to_com)
+         #tmp2=apply(as.matrix(tmp),1,.x2_to_com) 
+         #plot(tmp1,tmp2)
+        
+        lst1lst1=apply(as.matrix(DR[index1,THIS_DR]),1,.x1_to_com) 
+        lst2lst2=apply(as.matrix(DR[index2,THIS_DR]),1,.x2_to_com)        
         
         OUT$adr[index1,THIS_DR]=lst1lst1
         OUT$adr[index2,THIS_DR]=lst2lst2
@@ -160,6 +196,7 @@
         ALL_COR=c(ALL_COR, this_cor)
         ALL_PV=c(ALL_PV, this_pv) 
         print(THIS_DR)
+        
         THIS_DR=THIS_DR+1}
     
     #OUT$cor=ALL_COR
@@ -167,7 +204,7 @@
     OUT$pv=ALL_PV
     print('Finished!!!')
     return(OUT)
-    }
+   }
 
 
 
