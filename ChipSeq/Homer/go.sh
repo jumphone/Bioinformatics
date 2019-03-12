1d08773fc77a9d0358ba2838b38643b72c5f30d0
Zhang, Feng <jumphone@163.com>
2019-03-12 11:52:34 -0400

    Update go.sh

--- ChipSeq/Homer/go.sh
+++ ChipSeq/Homer/go.sh
@@ -8,4 +8,7 @@ export PATH=$PATH:/Users/zha8dh/Desktop/CCHMC_Project/LIJUN/Motif_20190312/bin
 
 findMotifsGenome.pl Lijun.txt.bed.bed mm10 OUTPUT -size 200
 
+nohup findMotifsGenome.pl Lijun.txt.bed.POS.bed mm10 OUTPUT_POS -size 200 &
+
+nohup findMotifsGenome.pl Lijun.txt.bed.NEG.bed mm10 OUTPUT_NEG -size 200 &
 
