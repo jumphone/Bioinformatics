1aba766ee7829082b6fc7ef8cc4bf10cf872fe74
Zhang, Feng <jumphone@163.com>
2019-03-12 11:22:49 -0400

    Update go.sh

--- ChipSeq/Homer/go.sh
+++ ChipSeq/Homer/go.sh
@@ -1,1 +1,9 @@
+#
+#http://homer.ucsd.edu/homer/
+
+perl configureHomer.pl -install homer
+perl /Users/zha8dh/Desktop/CCHMC_Project/LIJUN/Motif_20190312/.//configureHomer.pl -install mm10
+
+./bin/findMotifsGenome.pl Lijun.txt.bed.bed mm10 OUTPUT -size 200
+
 
