a4538e844d2e64d1ca0bcedfe0643cc864e307bc
Zhang, Feng <jumphone@163.com>
2019-03-18 15:26:56 -0400

    Update go.R

--- scRNAseq/Rohit_20190318/go.R
+++ scRNAseq/Rohit_20190318/go.R
@@ -5,3 +5,14 @@ source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 
 mybeer=MBEER(DATA, BATCH, MAXBATCH="", CNUM=10, PCNUM=20, CPU=2, SEED=1 )
 
+PCUSE <- which(mybeer$cor> 0.6  & mybeer$fdr<0.05)
+
+pbmc=mybeer$seurat
+
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims.use = PCUSE, check_duplicates=FALSE)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1)  
+
+
+saveRDS(pbmc,'pbmc.RDS')
+saveRDS(mybeer,'mybeer.RDS')
+
