7035d9e51f144fe7be067bf29ac4e5b2aa3cad0f
Zhang, Feng <jumphone@163.com>
2019-03-20 15:37:38 -0400

    Update go.R

--- RNAseqEXP/RAP_20190319/go.R
+++ RNAseqEXP/RAP_20190319/go.R
@@ -5,16 +5,36 @@ library(dplyr)
 pbmc.data <-read.table('combined.txt',sep='\t',row.names=1,header=T)
 
 pbmc <- CreateSeuratObject(raw.data = pbmc.data, min.cells = 0, min.genes = 0, project = "10X_PBMC")
+pbmc <- ScaleData(object = pbmc, vars.to.regress = c("nUMI"))
+
 
 V=apply(pbmc@data,1,var)
     
 used_gene=rownames(pbmc@data)[which(V>=quantile(V, 0.8))]
-
-
-pbmc <- ScaleData(object = pbmc, vars.to.regress = c("nUMI"))
 pbmc <- RunPCA(object = pbmc, pc.genes = used_gene, do.print = TRUE, pcs.print = 1:5, genes.print = 5)
 pbmc@meta.data$name=colnames(pbmc@data)
 
+
+
+pdf('PCA08.pdf',width=14,height=12)
 PCAPlot(object = pbmc, dim.1 = 1, dim.2 = 2)
 PCAPlot(object = pbmc, dim.1 = 1, dim.2 = 2,group.by='name', do.label=T)
+dev.off()
 
+
+
+
+    
+used_gene=rownames(pbmc@data)[which(V>0)]
+
+
+
+pbmc <- RunPCA(object = pbmc, pc.genes = used_gene, do.print = TRUE, pcs.print = 1:5, genes.print = 5)
+pbmc@meta.data$name=colnames(pbmc@data)
+
+
+
+pdf('PCA_ALL.pdf',width=14,height=12)
+PCAPlot(object = pbmc, dim.1 = 1, dim.2 = 2)
+PCAPlot(object = pbmc, dim.1 = 1, dim.2 = 2,group.by='name', do.label=T)
+dev.off()
