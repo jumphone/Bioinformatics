27dd84afc1048b3a3e1b203137040287dcf34c61
Zhang, Feng <jumphone@163.com>
2019-03-21 18:11:44 -0400

    Update go_new.R

--- scRNAseq/ZhengZhang20181211/go_new.R
+++ scRNAseq/ZhengZhang20181211/go_new.R
@@ -16,7 +16,7 @@ pdf('WT_QC.pdf')
 VlnPlot(object = pbmc, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
 dev.off()
 pbmc <- FilterCells(object = pbmc, subset.names = c("nGene", "percent.mito"), 
-    low.thresholds = c(200, -Inf), high.thresholds = c(4000, 0.5))
+    low.thresholds = c(200, -Inf), high.thresholds = c(7000, 1))
 pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", 
     scale.factor = 10000)
 
@@ -33,7 +33,7 @@ PCNUM=50
 #    genes.print = 5)
 pbmc <- RunPCA(object = pbmc, pc.genes = pbmc@var.genes,pcs.compute = PCNUM, do.print = F, pcs.print = 1:5, 
     genes.print = 5)
-pdf('WT_PCA.pdf')
+pdf('PCA.pdf')
 PCElbowPlot(object = pbmc)
 dev.off()
 
@@ -41,7 +41,7 @@ PCUSE=1:20
 pbmc <- RunTSNE(object = pbmc, dims.use = PCUSE, do.fast = TRUE)
 TSNEPlot(object = pbmc,pt.size=0.5)
 
-pdf('WT_TSNE.pdf')
+pdf('TSNE.pdf')
 TSNEPlot(object = pbmc,pt.size=0.5)
 dev.off()
 
@@ -55,7 +55,7 @@ dev.off()
 ##
 
 library(Seurat)
-source('scRef.R')
+#source('scRef.R')
 
 
 COL=c()
@@ -69,34 +69,22 @@ exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
 
 
 
-exp_ref_mat=read.table('MCA_SmallIntestine_ref_mouse.txt',header=T,row.names=1,sep='\t')
+exp_ref_mat=read.table('MCA_INTEST.txt',header=T,row.names=1,sep='\t')
 REF_TAG=colnames(exp_ref_mat)
 tmp=strsplit(REF_TAG, "_")
 REF_TAG=c()
 for(one in tmp){REF_TAG=c(REF_TAG, one[1])}
 NewRef=.generate_ref(exp_ref_mat, cbind(REF_TAG,REF_TAG), min_cell=1) 
 out=SCREF(exp_sc_mat, NewRef)
-pbmc@meta.data$MCASmallIntest=out$tag2[,2]
+pbmc@meta.data$mca=out$tag2[,2]
 
-pdf('WT_MCASmallIntest.pdf',width=14,height=10)
-TSNEPlot(object = pbmc, do.label=T, group.by ='MCASmallIntest', pt.size = 0.5)
+pdf('CellType.pdf',width=14,height=10)
+TSNEPlot(object = pbmc, do.label=T, group.by ='mca', pt.size = 0.5)
 dev.off()
 
 
 
 
-exp_ref_mat=read.table('TM_Large_Intestine_ref_mouse.txt',header=T,row.names=1,sep='\t')
-REF_TAG=colnames(exp_ref_mat)
-tmp=strsplit(REF_TAG, "_")
-REF_TAG=c()
-for(one in tmp){REF_TAG=c(REF_TAG, one[3])}
-NewRef=.generate_ref(exp_ref_mat, cbind(REF_TAG,REF_TAG), min_cell=1) 
-out=SCREF(exp_sc_mat, NewRef)
-pbmc@meta.data$TMLargeIntest=out$tag2[,2]
-
-pdf('WT_TMLargeIntest.pdf',width=14,height=10)
-TSNEPlot(object = pbmc, do.label=T, group.by ='TMLargeIntest', pt.size = 0.5)
-dev.off()
 
 
 saveRDS(pbmc,file='WT.RDS')
