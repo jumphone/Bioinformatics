65f78be7bdebbc3ca88aa1dd08773bdbc75a53fc
Zhang, Feng <jumphone@163.com>
2019-03-21 17:12:37 -0400

    Update ForNatalie.R

--- scRNAseq/Natalie_20190321/ForNatalie.R
+++ scRNAseq/Natalie_20190321/ForNatalie.R
@@ -1,3 +1,25 @@
+library('Seurat')
+pbmc=readRDS('GFPSN_merged.RDS')
+
+
+#MCA
+table(pbmc@meta.data$mca)
+TSNEPlot(object = pbmc, do.label=T, label.size=2.2, group.by ='mca')
+
+
+#Development
+table(pbmc@meta.data$dev)
+COLOR=heat.colors(n=length(table(pbmc@meta.data$dev))+2)
+TSNEPlot(object = pbmc, colors.use=COLOR, group.by ='dev')
+
+#Inj_bulk
+table(pbmc@meta.data$injbulk)
+TSNEPlot(object = pbmc, do.label=T, label.size=2.2, group.by ='injbulk')
+
+#Inj_sc
+table(pbmc@meta.data$injsc)
+TSNEPlot(object = pbmc, do.label=T, label.size=2.2, group.by ='injsc')
+
 
 
 
