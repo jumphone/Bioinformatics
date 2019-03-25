26003dd449024c1a66d44476f4e98c8b5277d010
Zhang, Feng <jumphone@163.com>
2019-03-25 16:51:30 -0400

    Update go_beer.R

--- scRNAseq/Natalie_20190321/go_beer.R
+++ scRNAseq/Natalie_20190321/go_beer.R
@@ -27,21 +27,10 @@ DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1)
 
 
 
-pbmc <- RunTSNE(object = pbmc, reduction.use='pca',dims.use = PCUSE, do.fast=T, check_duplicates=FALSE)
-DimPlot(pbmc, reduction.use='tsne', group.by='batch', pt.size=0.1)
+#pbmc <- RunTSNE(object = pbmc, reduction.use='pca',dims.use = PCUSE, do.fast=T, check_duplicates=FALSE)
+#DimPlot(pbmc, reduction.use='tsne', group.by='batch', pt.size=0.1)
 #DimPlot(pbmc, reduction.use='tsne', group.by='map', pt.size=0.1)
 
-
-VEC=pbmc@dr$umap@cell.embeddings
-
-# Here, we use the "dbscan" function to do clustering.
-library("fpc")
-set.seed(123)
-df=VEC
-db <- fpc::dbscan(df, eps = 0.5, MinPts = 5)
-DC=db$cluster
-pbmc@meta.data$DC=DC
-DimPlot(pbmc, reduction.use='umap', group.by='DC', pt.size=0.5)
 
 ##################
 
@@ -68,4 +57,50 @@ DoHeatmap(object = pbmc, genes.use = top10$gene, slim.col.label = TRUE, remove.k
 ############
 
 
-save(file='combine_beer.RData')
+save.image(file='combine_beer.RData')
+
+
+pdf('BEER.pdf',width=15,height=12)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1)
+DimPlot(pbmc, reduction.use='umap', group.by='DC', pt.size=0.5)
+DoHeatmap(object = pbmc, genes.use = top10$gene, slim.col.label = TRUE, remove.key = TRUE)
+dev.off()
+
+########################
+
+exp_sc_mat=d2
+load('Seurat_EXP_cluster.Robj')
+
+pbmc=EXP_cluster
+ref_vec=pbmc@dr$tsne@cell.embeddings
+
+COL=c()
+i=1
+while(i <=length(pbmc@ident)){
+    this_col=which(colnames(pbmc@raw.data)==names(pbmc@ident)[i])
+    COL=c(COL,this_col)
+    i=i+1
+    }
+exp_ref_mat=as.matrix(pbmc@raw.data)[,COL]
+ref_tag=cbind(names(pbmc@ident),as.character(pbmc@ident))    
+HQRef= .generate_ref(exp_ref_mat, ref_tag, min_cell = 10 )   
+
+sc_tag=SCREF(exp_sc_mat, HQRef)$tag2
+
+out =.vec_projection(exp_sc_mat, sc_tag, exp_ref_mat, ref_tag, ref_vec, 
+        method='kendall', nearest_cell=3, alpha=0.5, random_size=30, 
+        random_seed=123, CPU=4, print_step=10)
+
+########
+pdf('OVERLAY.pdf',width=15,height=12)
+XLIM=c(min(ref_vec[,1]-1),max(ref_vec[,1]+1))
+YLIM=c(min(ref_vec[,2]-1),max(ref_vec[,2]+1))
+plot(ref_vec,xlim=XLIM,ylim=YLIM,pch=16,col='grey70')
+par(new=T)
+plot(out$vec,xlim=XLIM,ylim=YLIM,pch=16,col='red')
+dev.off()
+
+saveRDS(out,'overlay.RDS')
+saveRDS(sc_tag,'sc_tag.RDS')
+
+
