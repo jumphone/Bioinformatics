231d6ae349198a0320d7cf9ed63a739dac6ecb84
Zhang, Feng <jumphone@163.com>
2019-04-18 16:38:35 -0400

    Create go.R

---
+++ Palantir/go.R
@@ -0,0 +1,36 @@
+import palantir
+
+# Plotting and miscellaneous imports
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+palantir_dir = os.path.expanduser('./')
+counts = palantir.io.from_csv(palantir_dir + 'MGH54_mat.txt',delimiter='\t')
+
+fig, ax = palantir.plot.plot_molecules_per_cell_and_gene(counts)
+plt.show()
+
+
+norm_df = palantir.preprocess.normalize_counts(counts)
+norm_df = palantir.preprocess.log_transform(norm_df)
+
+pca_projections, _ = palantir.utils.run_pca(norm_df)
+dm_res = palantir.utils.run_diffusion_maps(pca_projections, n_components=5)
+ms_data = palantir.utils.determine_multiscale_space(dm_res)
+tsne = palantir.utils.run_tsne(ms_data)
+
+
+fig, ax = palantir.plot.plot_tsne(tsne)
+plt.show()
+
+
+###########
+import pickle 
+pickle.dump(xxx, open('XXX.obj','w'))
+
+###########
+xxx = pickle.load(open('XXX.obj'))
+
+
