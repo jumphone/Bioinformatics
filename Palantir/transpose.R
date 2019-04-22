cb19a5766a43d575904da05b22b898974139af02
Zhang, Feng <jumphone@163.com>
2019-04-22 14:08:59 -0400

    Update transpose.R

--- Palantir/transpose.R
+++ Palantir/transpose.R
@@ -2,6 +2,6 @@ a=read.table('MGH54_mat.txt',header=T,row.names=1,sep='\t')
 b=t(a)
 
 varC=apply(b,2,var)
-varR=apply(b,1,var)
 
-write.table(b[which(varR!=0),which(varC!=0)],file='t_MGH54_mat.txt',row.names=T,col.names=T,sep=',',quote=F)
+
+write.table(b[,which(varC>0.5)],file='t_MGH54_mat.txt',row.names=T,col.names=T,sep=',',quote=F)
