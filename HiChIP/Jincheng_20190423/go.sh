dd23e63098e364cc5b1f57e162a428a47e1b3593
Zhang, Feng <jumphone@163.com>
2019-04-23 16:21:43 -0400

    Update go.sh

--- HiChIP/Jincheng_20190423/go.sh
+++ HiChIP/Jincheng_20190423/go.sh
@@ -30,25 +30,31 @@ sh cool_inter_mouse.sh Auxin2days.1000k.cool
 
 
 library(HiCcompare)
-con1000kb <- read.table("Untreated.1000k.cool.inter.txt", header = FALSE)
-aux1000kb <- read.table("Auxin2days.1000k.cool.inter.txt", header = FALSE)
+CHR=c('chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15',
+'chr16','chr17','chr18','chr19','chrX')
+OUT='./OUT/'
 
-chr1.table <- create.hic.table(con1000kb,aux1000kb,chr='chr1')
+for(this_chr in CHR){
+#this_chr='chr1'
+con1000kb <- read.table(paste0("Untreated.1000k.cool.tmp/",this_chr,'.txt'), header = FALSE)
+aux1000kb <- read.table(paste0("Auxin2days.1000k.cool.tmp/",this_chr,'.txt'), header = FALSE)
 
 
-hic.table <- hic_loess(chr1.table, Plot = TRUE, Plot.smooth = FALSE)
-
+this.table <- create.hic.table(con1000kb, aux1000kb)
+pdf(paste0(OUT,this_chr,'_result.pdf'),width=10,height=7)
+hic.table <- hic_loess(this.table, Plot = TRUE, Plot.smooth = FALSE)
 filter_params(hic.table)
 hic.table <- hic_compare(hic.table, A.min = 15, adjust.dist = TRUE, p.method = 'fdr', Plot = TRUE)
+dev.off()
 
-knitr::kable(head(hic.table))
+#knitr::kable(head(hic.table))
 sig_index=which(hic.table$p.adj<0.05)
 
 sig.hic.table=hic.table[sig_index,]
-knitr::kable(head(sig.hic.table))
-
-
-
+#knitr::kable(head(sig.hic.table))
+write.table(sig.hic.table,file=paste0(OUT,this_chr,'_sig.txt'),quote=F,sep='\t',row.names=F,col.names=T)
+print(this_chr)
+}
 
 
 
