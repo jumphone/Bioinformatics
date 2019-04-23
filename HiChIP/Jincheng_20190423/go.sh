698bbc51c8c1ba382293a6cf566ad2458b22bd00
Zhang, Feng <jumphone@163.com>
2019-04-23 16:23:50 -0400

    Update go.sh

--- HiChIP/Jincheng_20190423/go.sh
+++ HiChIP/Jincheng_20190423/go.sh
@@ -66,52 +66,6 @@ print(this_chr)
 
 
 
-#R
-#https://bioconductor.org/packages/release/bioc/vignettes/HiCcompare/inst/doc/HiCcompare-vignette.html
-#if (!requireNamespace("BiocManager", quietly=TRUE))
-#    install.packages("BiocManager")
-#BiocManager::install("HiCcompare")
-
-
-library(HiCcompare)
-aux1000kb <- read.table("GSM2644947_Auxin2days-R1.1000k.cool.chr1.txt", header = FALSE)
-con1000kb <- read.table("GSM2644945_Untreated-R1.1000k.cool.chr1.txt", header = FALSE)
-
-chr1.table <- create.hic.table(aux1000kb, con1000kb, chr = 'chr1')
-head(chr1.table)
-
-hic.table <- hic_loess(chr1.table, Plot = TRUE, Plot.smooth = FALSE)
-
-knitr::kable(head(hic.table))
-filter_params(hic.table)
-hic.table <- hic_compare(hic.table, A.min = 15, adjust.dist = TRUE, p.method = 'fdr', Plot = TRUE)
-
-knitr::kable(head(hic.table))
-sig_index=which(hic.table$p.adj<0.05)
-
-sig.hic.table=hic.table[sig_index,]
-knitr::kable(head(sig.hic.table))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
