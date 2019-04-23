08ce1eacdbc333b9ecf358af9f08bfec87ee9144
Zhang, Feng <jumphone@163.com>
2019-04-23 13:07:17 -0400

    Create download.sh

---
+++ HiChIP/Jincheng_20190423/download.sh
@@ -0,0 +1,6 @@
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM2644945
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM2644946
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM2644947
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM2644948
+
+sudo pip install cooler
