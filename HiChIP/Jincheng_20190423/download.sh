bbdf920f87fac173f53b2ec7eaebeeb168350649
Zhang, Feng <jumphone@163.com>
2019-04-23 13:10:58 -0400

    Update download.sh

--- HiChIP/Jincheng_20190423/download.sh
+++ HiChIP/Jincheng_20190423/download.sh
@@ -4,3 +4,6 @@ https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM2644947
 https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM2644948
 
 sudo pip install cooler
+
+
+cooler dump -b -t pixels --header --join -r chr3:10M-12M -r2 chr17  GSM2644947_Auxin2days-R1.100000.cool | less
