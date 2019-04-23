64ade4ea1342d91390c65f4a731c3e7d447d84df
Zhang, Feng <jumphone@163.com>
2019-04-23 14:51:41 -0400

    Update go.sh

--- HiChIP/Jincheng_20190423/go.sh
+++ HiChIP/Jincheng_20190423/go.sh
@@ -6,6 +6,18 @@ https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM2644948
 sudo pip install cooler
 
 
-cooler dump -b -t pixels --header --join -r chr3:10M-12M -r2 chr17  GSM2644947_Auxin2days-R1.100000.cool | less
+#cooler dump -b -t pixels --header --join -r chr3:10M-12M -r2 chr17  GSM2644947_Auxin2days-R1.100000.cool | less
+#cooler dump -b -t pixels --join GSM2644945_Untreated-R1.100000.cool > GSM2644945_Untreated-R1.100000.cool.bed
 
-cooler dump -b -t pixels --join GSM2644945_Untreated-R1.100000.cool > GSM2644945_Untreated-R1.100000.cool.bed
+cooler coarsen -k 10 GSM2644945_Untreated-R1.100000.cool -o GSM2644945_Untreated-R1.1000k.cool
+cooler dump --join GSM2644945_Untreated-R1.1000k.cool > GSM2644945_Untreated-R1.1000k.cool.bed
+
+
+
+if (!requireNamespace("BiocManager", quietly=TRUE))
+    install.packages("BiocManager")
+BiocManager::install("HiCcompare")
+library(HiCcompare)
+
+
+hesc100kb <- read.table("GSM2644945_Untreated-R1.100000.cool.bed", header = FALSE)
