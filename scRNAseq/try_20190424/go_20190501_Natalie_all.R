77fc3d58c58ffb4d75cfd37eba167f4148c42a76
Zhang, Feng <jumphone@163.com>
2019-05-01 13:28:42 -0400

    Update go_20190501_Natalie_all.R

--- scRNAseq/try_20190424/go_20190501_Natalie_all.R
+++ scRNAseq/try_20190424/go_20190501_Natalie_all.R
@@ -8,8 +8,8 @@ library(Matrix)
 
 load('Seurat_EXP_cluster.Robj')
 
-pbmc.raw.data=as.matrix(EXP_cluster@raw.data[,which(colnames(EXP_cluster@raw.data) %in% colnames(EXP_cluster@scale.data))])
-pbmc.data=as.matrix(EXP_cluster@scale.data)
+#pbmc.raw.data=as.matrix(EXP_cluster@raw.data[,which(colnames(EXP_cluster@raw.data) %in% colnames(EXP_cluster@scale.data))])
+#pbmc.data=as.matrix(EXP_cluster@scale.data)
 
 #source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 #ONE=.data2one(pbmc.raw.data, rownames(pbmc.data), CPU=4, PCNUM=50, SEED=123,  PP=30)
@@ -126,3 +126,43 @@ colnames(LP)=paste0(colnames(LP),'_',c(LT,RT))
 write.table(LP,file=paste0(LT,'_',RT,'.tsv'),col.names=T,row.names=T,quote=F,sep='\t')
 dev.off()
 ###################
+
+
+
+###################
+LT='1.5mo_53,41,40_tumorSCs'
+RT='30,32_T cell'
+ORITAG=rep('NA',length(ONE))
+ORITAG[which(BINTAG %in% c(53,41,40))]=LT
+ORITAG[which(BINTAG %in% c(30,32))]=RT
+
+NET=getNET(PAIR[1:200,], BINTAG,ORITAG )
+CN=getCN(NET)
+DP=DPlot(NET, CN,COL=3,PLOT=F)
+pdf(paste0(LT,'_to_',RT,'.pdf'),width=12,height=12)
+LP=LPlot(LT,RT,NET,PMAT,SEED=12)
+colnames(LP)=paste0(colnames(LP),'_',c(LT,RT))
+write.table(LP,file=paste0(LT,'_',RT,'.tsv'),col.names=T,row.names=T,quote=F,sep='\t')
+dev.off()
+###################
+
+
+###################
+LT='4mo_47,48,50_tumorSCs'
+RT='30,31,32_T cell'
+ORITAG=rep('NA',length(ONE))
+ORITAG[which(BINTAG %in% c(47,48,50))]=LT
+ORITAG[which(BINTAG %in% c(30,31,32))]=RT
+
+NET=getNET(PAIR[1:200,], BINTAG,ORITAG )
+CN=getCN(NET)
+DP=DPlot(NET, CN,COL=3,PLOT=F)
+pdf(paste0(LT,'_to_',RT,'.pdf'),width=12,height=12)
+LP=LPlot(LT,RT,NET,PMAT,SEED=12)
+colnames(LP)=paste0(colnames(LP),'_',c(LT,RT))
+write.table(LP,file=paste0(LT,'_',RT,'.tsv'),col.names=T,row.names=T,quote=F,sep='\t')
+dev.off()
+
+
+
+
