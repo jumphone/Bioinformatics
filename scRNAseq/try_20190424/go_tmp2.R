c2557577f5413b8f76c8af6cef0859c8a10fa195
Zhang, Feng <jumphone@163.com>
2019-05-02 16:07:07 -0400

    Update go_tmp2.R

--- scRNAseq/try_20190424/go_tmp2.R
+++ scRNAseq/try_20190424/go_tmp2.R
@@ -36,7 +36,7 @@ saveRDS(MEAN,file='MEAN.RDS')
 PMAT=getPMAT(EXP, LR, BIN, MEAN)
 saveRDS(PMAT,file='PMAT.RDS')
 
-CMAT=getCMAT(EXP,LR,PMAT)
+CMAT=getCMAT(EXP,LR,PMAT,PRO=FALSE)
 saveRDS(CMAT,file='CMAT.RDS')
 
 pdf('2HEAT.pdf',width=15,height=13)
