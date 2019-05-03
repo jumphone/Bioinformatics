c452c06bc803d67ac419ee5a7b90212d4c21bbfe
Zhang, Feng <jumphone@163.com>
2019-05-03 17:08:23 -0400

    Update go_tmp2.R

--- scRNAseq/try_20190424/go_tmp2.R
+++ scRNAseq/try_20190424/go_tmp2.R
@@ -11,6 +11,122 @@ pbmc.raw.data=as.matrix(pbmc@raw.data[,which(colnames(pbmc@raw.data) %in% colnam
 pbmc.data=as.matrix(pbmc@scale.data)
 used_gene=rownames(pbmc.data)
 
+    source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')  
+    #---------------------------------------
+    # For Seurat==3.0, please use:
+    # source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER_Seurat3.R')
+    #---------------------------------------
+    
+    ONE=.data2one(pbmc.raw.data, used_gene, CPU=4, PCNUM=50, SEED=123,  PP=30)
+    saveRDS(ONE,file='ONE.RDS')
+    
+    OUT=getBIN(ONE)
+    BIN=OUT$BIN
+    BINTAG=OUT$TAG
+    saveRDS(BIN,file='BIN.RDS')
+    saveRDS(BINTAG,file='BINTAG.RDS')
+
+
+
+EXP=pbmc.data
+LR=read.table('RL_mouse.txt',header=T,sep='\t')
+
+ 
+    MEAN=getMEAN(EXP, LR)
+    saveRDS(MEAN,file='MEAN.RDS')
+        
+    PMAT=getPMAT(EXP, LR, BIN, MEAN)
+    saveRDS(PMAT,file='PMAT.RDS')
+    
+
+OUT=getPmatHEAT(PMAT,SHOW=T)
+HEAT=OUT$HEAT
+DIST=OUT$DIST
+
+
+
+CLUST=getCLUST(HEAT, DIST, CCUT=0.7, SHOW=T)
+
+
+MLR=getMLR(CLUST, LR, PMAT)
+LR=MLR[,c(1:2)]
+
+
+
+
+CMAT=getCMAT(EXP,LR,PMAT,BI=T)
+saveRDS(CMAT,file='CMAT.RDS')
+
+pdf('2HEAT.pdf',width=15,height=13)
+library('gplots')
+heatmap.2(CMAT,scale=c("none"),dendrogram='none',Colv=F,Rowv=F,trace='none',
+  col=colorRampPalette(c('blue3','grey95','red3')) ,margins=c(10,15))
+dev.off()
+
+
+OUT=getPAIR(CMAT)
+PAIR=OUT$PAIR
+SCORE=OUT$SCORE
+RANK=OUT$RANK
+saveRDS(PAIR,file='PAIR.RDS')
+
+#---- !!! Changed in Seurat 3.0 !!! ----
+VEC=pbmc@dr$umap@cell.embeddings
+#--------------------------------------- 
+# For Seurat 3.0, please use:
+# VEC=pbmc@reductions$tsne@cell.embeddings
+#---------------------------------------
+
+pdf('3CPlot.pdf',width=12,height=10)
+CPlot(VEC,PAIR[1:200,],BINTAG)
+dev.off()
+
+
+ORITAG=as.character(pbmc@ident)
+ORITAG[which(ORITAG=='Pericyte/\nFibroblast')]='Pericyte Fibroblast'
+
+NET=getNET(PAIR, BINTAG,ORITAG )
+write.table(NET,file='NET.txt',sep='\t',row.names=F,col.names=T,quote=F)
+   
+CN=getCN(NET)
+pdf('4DPlot.pdf',width=20,height=20)
+DP=DPlot(NET, CN, COL=3)
+dev.off()
+
+
+SIG_INDEX=which(DP<0.05)
+SIG_PAIR=names(SIG_INDEX)
+
+pdf('5LPlot.pdf',width=20,height=20)
+RCN=trunc(sqrt(length(SIG_PAIR))+1)
+par(mfrow=c(RCN,RCN))
+i=1
+while(i<= length(SIG_PAIR) ){
+    this_pair=SIG_PAIR[i]
+    LT=unlist(strsplit(this_pair, "_to_"))[1]
+    RT=unlist(strsplit(this_pair, "_to_"))[2]
+    LP=LPlot(LT, RT, NET, PMAT=PMAT,LR=LR,MAIN=SIG_INDEX[i],SEED=12345, PCUT=0.05)
+    colnames(LP)=paste0(c('Lexp','Rexp'),'_',c(LT,RT))
+    write.table(LP,file=paste0(as.character(SIG_INDEX[i]),'.tsv'),row.names=T,col.names=T,sep='\t',quote=F)
+    print(i)
+    i=i+1}
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 ONE=.data2one(pbmc.raw.data, used_gene, CPU=4, PCNUM=50, SEED=123,  PP=30)
 #ONE=readRDS('ONE.RDS')
