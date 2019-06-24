801ed3cfe55d4feb23b5084366c0a68acadb85e9
Zhang, Feng <jumphone@163.com>
2019-06-24 14:38:12 -0400

    Update go_new_20190618.R

--- scRNAseq/AGE_20190530/go_new_20190618.R
+++ scRNAseq/AGE_20190530/go_new_20190618.R
@@ -91,16 +91,12 @@ write.table(PT,'PT.cls',sep=' ',quote=F,row.names=F,col.names=F )
 
 
 
+write.table(rownames(pbmc),file='RN.txt',quote=F,sep='\t',row.names=F,col.names=F)
 
-
-USED_GENE1=c('Wnt1','Wnt2','Wnt3','Wnt3a','Wnt8a','Wnt8b','Wnt10a','Wnt10b',
-             'Ascl2', 'Axin2', 'Olfm4', 'Ctnnb1', 'Ephb2', 'Cd44', 'Myc')
+USED_GENE1=c('Wnt1','Wnt2','Wnt3','Wnt3a','Wnt8a','Wnt8b','Wnt10a','Wnt10b')
 
 USED_GENE2=c('Wnt4', 'Wnt5a', 'Wnt5b', 'Wnt6', 'Wnt7a', 'Wnt7b', 'Wnt9a', 'Wnt9b', 'Wnt11')
    
-
-
-
 
 Olfm4Cell=which(pbmc@meta.data$newall=='Stem')
 
@@ -117,7 +113,8 @@ COL=rep('blue',ncol(Olfm4EXP))
 COL[which(Olfm4META$batch=='Aged')]='red'
 
 library('gplots')
-heatmap.2(Olfm4EXP[which(rownames(Olfm4EXP) %in% USED_GENE1),],ColSideColors=COL,labCol='',scale=c("none"),dendrogram='none',Rowv=F,Colv=F,trace='none',col=colorRampPalette(c('grey80','red')),margins=c(5,5))
+wnt3=which(rownames(Olfm4EXP) %in% USED_GENE1)
+heatmap.2(Olfm4EXP[c(wnt3,wnt3),],ColSideColors=COL,labCol='',scale=c("none"),dendrogram='none',Rowv=F,Colv=F,trace='none',col=colorRampPalette(c('grey80','red')),margins=c(5,5))
 
 heatmap.2(Olfm4EXP[which(rownames(Olfm4EXP) %in% USED_GENE2),],ColSideColors=COL,labCol='',scale=c("none"),dendrogram='none',Rowv=F,Colv=F,trace='none',col=colorRampPalette(c('grey80','red')),margins=c(5,10))
 
@@ -125,10 +122,14 @@ dev.off()
 
 
 
+LB=rep('Young',length(COL))
+LB[which(COL=='red')]='Aged'
+
+OUT=cbind(Olfm4EXP[which(rownames(Olfm4EXP) %in% 'Wnt5b'),],LB)
+write.table(OUT,'WNT5B.txt',sep='\t',quote=F,row.names=F,col.names=F )
 
 
-
-USED_GENE3=c('Ascl2', 'Axin2', 'Myc', 'Olfm4')
+#USED_GENE3=c('Ascl2', 'Axin2', 'Myc', 'Olfm4')
 
 
 Olfm4Cell=which(pbmc@meta.data$newall=='Stem')
@@ -145,4 +146,74 @@ library('gplots')
 
 heatmap.2(Olfm4EXP[which(rownames(Olfm4EXP) %in% USED_GENE3),],ColSideColors=COL,labCol='',scale=c("none"),
           dendrogram='none',Rowv=F,Colv=F,trace='none',col=colorRampPalette(c('grey80','red')),margins=c(5,10))
+
+
+
+
+SE=apply(Olfm4EXP[which(rownames(Olfm4EXP) %in% USED_GENE3),],1,scale)
+MSE=apply(SE,1,mean)
+boxplot(MSE~COL)
+
+
+
+
+
+
+
+#########
+
+
+
+STEM=which(pbmc@meta.data$newall=='Paneth')#which( pbmc@meta.data$all %in% c('TA.Early','TA.G1','TA.G2') &  pbmc@assays$RNA@data[which(rownames(pbmc)=='Olfm4'),] >0)
+EXP=as.matrix(pbmc@assays$RNA@data[,STEM])
+VAR=apply(EXP,1,var)
+EXP=EXP[which(VAR>0),]
+PT=t(as.character(pbmc@meta.data$batch[STEM]))
+OUT=cbind(toupper(rownames(EXP)),rep('NO',nrow(EXP)),EXP)
+colnames(OUT)[c(1,2)]=c('GENE','DESCRIPTION')
+write.table(OUT,'Pa_EXP.txt',sep='\t',quote=F,row.names=F,col.names=T)
+write.table(PT,'Pa_PT.cls',sep=' ',quote=F,row.names=F,col.names=F )
+
+
+
+#write.table(rownames(pbmc),file='RN.txt',quote=F,sep='\t',row.names=F,col.names=F)
+
+USED_GENE1=c('Wnt1','Wnt2','Wnt3','Wnt3a','Wnt8a','Wnt8b','Wnt10a','Wnt10b')
+
+USED_GENE2=c('Wnt4', 'Wnt5a', 'Wnt5b', 'Wnt6', 'Wnt7a', 'Wnt7b', 'Wnt9a', 'Wnt9b', 'Wnt11')
+   
+
+Olfm4Cell=which(pbmc@meta.data$newall=='Paneth')
+
+
+
+pdf("NEWNEWHEAT_Paneth.pdf",width=10,height=7)
+
+Olfm4EXP=as.matrix(pbmc@assays$RNA@data[,Olfm4Cell])
+VAR=apply(Olfm4EXP,1,var)
+Olfm4EXP=Olfm4EXP[which(VAR>0),]
+
+Olfm4META=pbmc@meta.data[Olfm4Cell,]
+COL=rep('blue',ncol(Olfm4EXP))
+COL[which(Olfm4META$batch=='Aged')]='red'
+LB=rep('Young',length(COL))
+LB[which(COL=='red')]='Aged'
+
+
+boxplot(Olfm4EXP[c(wnt3),]~LB)
+
+
+library('gplots')
+wnt3=which(rownames(Olfm4EXP) %in% USED_GENE1)
+heatmap.2(Olfm4EXP[c(wnt3,wnt3),],ColSideColors=COL,labCol='',scale=c("none"),dendrogram='none',Rowv=F,Colv=F,trace='none',col=colorRampPalette(c('grey80','red')),margins=c(10,5))
+
+Olfm4EXP[c(wnt3),]
+
+
+heatmap.2(Olfm4EXP[which(rownames(Olfm4EXP) %in% USED_GENE2),],ColSideColors=COL,labCol='',scale=c("none"),dendrogram='none',Rowv=F,Colv=F,trace='none',col=colorRampPalette(c('grey80','red')),margins=c(5,10))
+
+dev.off()
+
+
+
 
