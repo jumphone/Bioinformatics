5d61b3d3740762b030ab5072047650e0166b62dc
Zhang, Feng <jumphone@163.com>
2019-06-24 13:58:23 -0400

    Update go_new_20190618.R

--- scRNAseq/AGE_20190530/go_new_20190618.R
+++ scRNAseq/AGE_20190530/go_new_20190618.R
@@ -97,7 +97,11 @@ USED_GENE1=c('Wnt1','Wnt2','Wnt3','Wnt3a','Wnt8a','Wnt8b','Wnt10a','Wnt10b',
              'Ascl2', 'Axin2', 'Olfm4', 'Ctnnb1', 'Ephb2', 'Cd44', 'Myc')
 
 USED_GENE2=c('Wnt4', 'Wnt5a', 'Wnt5b', 'Wnt6', 'Wnt7a', 'Wnt7b', 'Wnt9a', 'Wnt9b', 'Wnt11')
-          
+   
+
+
+
+
 Olfm4Cell=which(pbmc@meta.data$newall=='Stem')
 
 
@@ -119,4 +123,26 @@ heatmap.2(Olfm4EXP[which(rownames(Olfm4EXP) %in% USED_GENE2),],ColSideColors=COL
 
 dev.off()
 
+
+
+
+
+
+USED_GENE3=c('Ascl2', 'Axin2', 'Myc', 'Olfm4')
+
+
+Olfm4Cell=which(pbmc@meta.data$newall=='Stem')
+
+Olfm4EXP=as.matrix(pbmc@assays$RNA@data[,Olfm4Cell])
+VAR=apply(Olfm4EXP,1,var)
+Olfm4EXP=Olfm4EXP[which(VAR>0),]
+
+Olfm4META=pbmc@meta.data[Olfm4Cell,]
+COL=rep('blue',ncol(Olfm4EXP))
+COL[which(Olfm4META$batch=='Aged')]='red'
+
+library('gplots')
+
+heatmap.2(Olfm4EXP[which(rownames(Olfm4EXP) %in% USED_GENE3),],ColSideColors=COL,labCol='',scale=c("none"),
+          dendrogram='none',Rowv=F,Colv=F,trace='none',col=colorRampPalette(c('grey80','red')),margins=c(5,10))
 
