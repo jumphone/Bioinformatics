e79be0cb38e3050cd060fed7f29acd49f998afe7
Zhang, Feng <jumphone@163.com>
2019-08-20 11:44:05 -0400

    Update go.R

--- scRNAseq/HongFangZi/go.R
+++ scRNAseq/HongFangZi/go.R
@@ -3,6 +3,20 @@ setwd('/users/zha8dh/tianlab/HFZ')
 
 D1=read.table(gzfile("./ALL_EXON_DGE/decidua0117_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
 D2=read.table(gzfile("./ALL_EXON_DGE/decidua0417-2_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D3=read.table(gzfile("./ALL_EXON_DGE/decidua508_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D4=read.table(gzfile("./ALL_EXON_DGE/decidua510_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D5=read.table(gzfile("./ALL_EXON_DGE/decidua514-2_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D6=read.table(gzfile("./ALL_EXON_DGE/decidua-2018_combined_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D7=read.table(gzfile("./ALL_EXON_DGE/decidua-2019_combined_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D8=read.table(gzfile("./ALL_EXON_DGE/decidua20190215_combined_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D9=read.table(gzfile("./ALL_EXON_DGE/Decidua20190420_L3_1000903_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D10=read.table(gzfile("./ALL_EXON_DGE/placenta0423-2_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D11=read.table(gzfile("./ALL_EXON_DGE/placenta508-1_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D12=read.table(gzfile("./ALL_EXON_DGE/placenta508-2_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D13=read.table(gzfile("./ALL_EXON_DGE/placenta514-2_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D14=read.table(gzfile("./ALL_EXON_DGE/placenta-2018_combined_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D15=read.table(gzfile("./ALL_EXON_DGE/placenta-2019_combined_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D16=read.table(gzfile("./ALL_EXON_DGE/Placenta20190402_L3_1000902_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
 
 
 
