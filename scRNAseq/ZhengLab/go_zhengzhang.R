d1eb6dc4313d4a6ecb399f9836b249673d58f8fa
Zhang, Feng <jumphone@163.com>
2019-08-20 15:41:23 -0400

    Update go_zhengzhang.R

--- scRNAseq/ZhengLab/go_zhengzhang.R
+++ scRNAseq/ZhengLab/go_zhengzhang.R
@@ -11,3 +11,22 @@ pbmc@meta.data$celltype=rep(NA,ncol(pbmc))
 pbmc@meta.data$celltype[which(pbmc@meta.data$batch=='NATURE')]=LABEL
 #DimPlot(pbmc, reduction.use='umap', group.by='celltype', pt.size=0.1,label=F)
 DimPlot(pbmc, reduction.use='umap', group.by='celltype', pt.size=0.1,label=T)
+
+
+
+
+USED_CELL=which(pbmc@meta.data$batch %in% c('CDC42KO','CDC42HET'))
+pbmc_zhengzhang <- CreateSeuratObject(counts = pbmc@assays$RNA@counts[,USED_CELL], project = "ZhengZhang", min.cells = 0, min.features = 0)
+pbmc_zhengzhang@meta.data=pbmc@meta.data[USED_CELL,]
+pbmc_zhengzhang <- NormalizeData(pbmc_zhengzhang, normalization.method = "LogNormalize", scale.factor = 10000)
+all.genes <- rownames(pbmc_zhengzhang)
+pbmc_zhengzhang <- ScaleData(pbmc_zhengzhang, features = all.genes)
+VariableFeatures(pbmc_zhengzhang)=VariableFeatures(pbmc)
+pbmc_zhengzhang <- RunPCA(pbmc_zhengzhang, npcs=10,features = VariableFeatures(object = pbmc_zhengzhang))
+pbmc_zhengzhang <- RunUMAP(pbmc_zhengzhang, dims = 1:10)
+pbmc_zhengzhang@reductions$umap@cell.embeddings=pbmc@reductions$umap@cell.embeddings[USED_CELL,]
+saveRDS(pbmc_zhengzhang,'pbmc_zhengzhang.RDS')
+
+
+
+
