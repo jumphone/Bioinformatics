f9b8cd9315b085eff7c09f04aea5ccc88a9cbeb8
Zhang, Feng <jumphone@163.com>
2019-08-20 11:33:02 -0400

    Update go.R

--- scRNAseq/HongFangZi/go.R
+++ scRNAseq/HongFangZi/go.R
@@ -1,4 +1,8 @@
 
 setwd('F:/Hongfangzi')
 
+D1=read.table(gzfile("./ALL_EXON_DGE/decidua0117_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+D2=read.table(gzfile("./ALL_EXON_DGE/decidua0417-2_exon_tagged.dge.txt.gz"),header=T,row.names=1)   
+
+
 
