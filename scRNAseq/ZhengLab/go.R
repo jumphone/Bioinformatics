30b9a554ea6ec993836d814082e1c5739b85d101
Zhang, Feng <jumphone@163.com>
2019-08-20 15:41:53 -0400

    Update go.R

--- scRNAseq/ZhengLab/go.R
+++ scRNAseq/ZhengLab/go.R
@@ -163,6 +163,11 @@ pbmc=readRDS('./pbmc1.RDS')
 LABEL=readRDS(file='LABEL.RDS')
 pbmc@meta.data$celltype=rep(NA,ncol(pbmc))
 pbmc@meta.data$celltype[which(pbmc@meta.data$batch=='NATURE')]=LABEL
+
+
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1,label=T)
+
+
 #DimPlot(pbmc, reduction.use='umap', group.by='celltype', pt.size=0.1,label=F)
 DimPlot(pbmc, reduction.use='umap', group.by='celltype', pt.size=0.1,label=T)
 
@@ -186,8 +191,12 @@ pbmc_zhengzhang <- ScaleData(pbmc_zhengzhang, features = all.genes)
 VariableFeatures(pbmc_zhengzhang)=VariableFeatures(pbmc)
 pbmc_zhengzhang <- RunPCA(pbmc_zhengzhang, npcs=10,features = VariableFeatures(object = pbmc_zhengzhang))
 pbmc_zhengzhang <- RunUMAP(pbmc_zhengzhang, dims = 1:10)
-
 pbmc_zhengzhang@reductions$umap@cell.embeddings=pbmc@reductions$umap@cell.embeddings[USED_CELL,]
+
+
+
+
+
 
 
 
