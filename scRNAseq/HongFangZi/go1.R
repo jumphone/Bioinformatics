45b848eeb51914336d220c618e93f19504c9ad63
Zhang, Feng <jumphone@163.com>
2019-08-20 12:11:39 -0400

    Update go1.R

--- scRNAseq/HongFangZi/go1.R
+++ scRNAseq/HongFangZi/go1.R
@@ -1,1 +1,13 @@
 
+
+setwd('/users/zha8dh/tianlab/HFZ')
+source('./BEER.R')
+
+DATA=readRDS('./DATA.RDS')
+BATCH=readRDS('./BATCH.RDS')
+
+mybeer=BEER(DATA, BATCH, GNUM=30, PCNUM=50, ROUND=1, GN=2000, SEED=1, COMBAT=TRUE, RMG=NULL)   
+
+
+
+
