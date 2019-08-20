a4b852cad49d47a99cb53c59af232d9a5877bd79
Zhang, Feng <jumphone@163.com>
2019-08-20 12:46:18 -0400

    Update go1.R

--- scRNAseq/HongFangZi/go1.R
+++ scRNAseq/HongFangZi/go1.R
@@ -18,18 +18,28 @@ source('./BEER.R')
 .set_python('~/anaconda3/bin/python3')
 mybeer=readRDS('./mybeer.RDS')
 
+pdf('~/Downloads/HFZ1.pdf',width=10,height=10)
+
 PCUSE=mybeer$select
 COL=rep('black',length(mybeer$cor))
 COL[PCUSE]='red'
 plot(mybeer$cor,mybeer$lcor,pch=16,col=COL,
     xlab='Rank Correlation',ylab='Linear Correlation',xlim=c(0,1),ylim=c(0,1))
 
+dev.off()
 
+#############
 pbmc <- mybeer$seurat
+################
+
+pdf('~/Downloads/HFZ2.pdf',width=10,height=10)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.5,label=F)
+dev.off()
+
+
 #pbmc=BEER.combat(pbmc) #Adjust PCs using ComBat
 umap=BEER.bbknn(pbmc, PCUSE, NB=5, NT=10)
 pbmc@reductions$umap@cell.embeddings=umap
-DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1,label=F)
-
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.5,label=F)
 
 
