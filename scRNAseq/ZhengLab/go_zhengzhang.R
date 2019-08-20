fb56ba6c229261637005277c9fa7354d1ec339ba
Zhang, Feng <jumphone@163.com>
2019-08-20 15:38:25 -0400

    Update go_zhengzhang.R

--- scRNAseq/ZhengLab/go_zhengzhang.R
+++ scRNAseq/ZhengLab/go_zhengzhang.R
@@ -3,12 +3,6 @@
 
 setwd('F:/Zhenglab/Combine')
 source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
-#.set_python('/Users/zha8dh/anaconda3/bin/python')
-
-#DATA=readRDS('DATA1.RDS') # Zheng Zhang
-#BATCH=readRDS('BATCH1.RDS')
-
-
 pbmc=readRDS('./pbmc1.RDS')
 
 
