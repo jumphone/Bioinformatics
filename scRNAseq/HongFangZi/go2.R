341f3d399367d378bcf944ce7500eb3745f137a8
Zhang, Feng <jumphone@163.com>
2019-08-20 17:17:44 -0400

    Update go2.R

--- scRNAseq/HongFangZi/go2.R
+++ scRNAseq/HongFangZi/go2.R
@@ -38,8 +38,8 @@ pbmc@meta.data$clust=as.character(CLUST)
 
 saveRDS(pbmc@meta.data, file='META_300.RDS')
 
-pdf('~/Downloads/HFZ5.CLUST.300.pdf',width=10,height=10)
-DimPlot(pbmc, reduction.use='umap', group.by='clust', pt.size=0.5,label=TRUE)
+pdf('~/Downloads/HFZ5.CLUST.300.pdf',width=20,height=20)
+DimPlot(pbmc, reduction.use='umap', group.by='clust', pt.size=0.5,label=TRUE) + NoLegend()
 dev.off()
 
 EXP_CLUST=.generate_mean(pbmc@assays$RNA@data,pbmc@meta.data$clust)
@@ -52,8 +52,13 @@ saveRDS(VEC_CLUST, file='./VEC_CLUST.RDS')
 saveRDS(EXP_CLUST, file='~/Downloads/EXP_CLUST.RDS')
 saveRDS(VEC_CLUST, file='~/Downloads/VEC_CLUST.RDS')
 
+pbmc.markers=readRDS(file='pbmc.markers.RDS')
+.writeTable(pbmc.markers,PATH='~/Downloads/Markers.txt')
 
-
-
+library(dplyr)
+top10 <- pbmc.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+top10=as.matrix(top10)
+rownames(top10)=c(1:nrow(top10))
+.writeTable(top10,PATH='~/Downloads/top10.txt')
 
 #####################
