df87844cc8f968743f3dfbb244a5c07de11619c3
Zhang, Feng <jumphone@163.com>
2019-08-20 12:29:13 -0400

    Update go1.R

--- scRNAseq/HongFangZi/go1.R
+++ scRNAseq/HongFangZi/go1.R
@@ -2,11 +2,20 @@
 
 setwd('/users/zha8dh/tianlab/HFZ')
 source('./BEER.R')
-
 DATA=readRDS('./DATA.RDS')
 BATCH=readRDS('./BATCH.RDS')
 
 mybeer=BEER(DATA, BATCH, GNUM=30, PCNUM=50, ROUND=1, GN=2000, SEED=1, COMBAT=TRUE, RMG=NULL)   
+
+saveRDS(mybeer,file='mybeer.RDS')
+
+
+##################################
+setwd('/users/zha8dh/tianlab/HFZ')
+source('./BEER.R')
+.set_python('')
+mybeer=readRDS('./mybeer.RDS')
+
 
 
 
