13327b7e814867604574a42bacbe94711c27a80a
Zhang, Feng <jumphone@163.com>
2019-08-20 15:38:16 -0400

    Update go_zhengzhang.R

--- scRNAseq/ZhengLab/go_zhengzhang.R
+++ scRNAseq/ZhengLab/go_zhengzhang.R
@@ -1,1 +1,19 @@
 
+
+
+setwd('F:/Zhenglab/Combine')
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+#.set_python('/Users/zha8dh/anaconda3/bin/python')
+
+#DATA=readRDS('DATA1.RDS') # Zheng Zhang
+#BATCH=readRDS('BATCH1.RDS')
+
+
+pbmc=readRDS('./pbmc1.RDS')
+
+
+LABEL=readRDS(file='LABEL.RDS')
+pbmc@meta.data$celltype=rep(NA,ncol(pbmc))
+pbmc@meta.data$celltype[which(pbmc@meta.data$batch=='NATURE')]=LABEL
+#DimPlot(pbmc, reduction.use='umap', group.by='celltype', pt.size=0.1,label=F)
+DimPlot(pbmc, reduction.use='umap', group.by='celltype', pt.size=0.1,label=T)
