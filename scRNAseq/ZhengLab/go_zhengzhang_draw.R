e70ebfaa1264d56a55500dc9803166cf16c5ecfb
Zhang, Feng <jumphone@163.com>
2019-08-20 16:14:35 -0400

    Create go_zhengzhang_draw.R

---
+++ scRNAseq/ZhengLab/go_zhengzhang_draw.R
@@ -0,0 +1,13 @@
+
+
+
+setwd('F:/Zhenglab/Combine')
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+pbmc=readRDS('./pbmc1.RDS')
+pbmc_zhengzhang=readRDS('pbmc_zhengzhang.RDS')
+
+pbmc@meta.data=readRDS('pbmc1_meta.RDS')
+pbmc_zhengzhang@meta.data=readRDS('pbmc_zhengzhang_meta.RDS')
+
+
+
