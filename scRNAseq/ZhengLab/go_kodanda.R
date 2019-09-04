b71270444b5bac96d30010d8052bb6c1a52644b6
Zhang, Feng <jumphone@163.com>
2019-09-04 16:17:24 -0400

    Update go_kodanda.R

--- scRNAseq/ZhengLab/go_kodanda.R
+++ scRNAseq/ZhengLab/go_kodanda.R
@@ -17,7 +17,8 @@ saveRDS(mybeer,file='mybeer2.RDS')
 
 
 
-PCUSE=mybeer$select
+#PCUSE=mybeer$select
+PCUSE=.selectUSE(mybeer, CUTR=0.8, CUTL=0.8, RR=0.5, RL=0.5)
 
 COL=rep('black',length(mybeer$cor))
 COL[PCUSE]='red'
@@ -26,8 +27,8 @@ plot(mybeer$cor,mybeer$lcor,pch=16,col=COL,
 
 
 pbmc <- mybeer$seurat  
-PCUSE=mybeer$select
-#PCUSE=.selectUSE(mybeer, CUTR=0.8, CUTL=0.8, RR=0.5, RL=0.5)
+#PCUSE=mybeer$select
+PCUSE=.selectUSE(mybeer, CUTR=0.8, CUTL=0.8, RR=0.5, RL=0.5)
 
 #pbmc=BEER.combat(pbmc) #Adjust PCs using ComBat
 umap=BEER.bbknn(pbmc, PCUSE, NB=5, NT=10)
@@ -40,6 +41,70 @@ saveRDS(pbmc,file='pbmc2.RDS')
 
 
 
+
+LABEL=readRDS(file='LABEL.RDS')
+pbmc@meta.data$celltype=rep(NA,ncol(pbmc))
+pbmc@meta.data$celltype[which(pbmc@meta.data$batch=='NATURE')]=LABEL
+DimPlot(pbmc, reduction.use='umap', group.by='celltype', pt.size=0.1,label=T)
+
+
+
+#######
+VEC=pbmc@reductions$umap@cell.embeddings
+set.seed(321)
+N=100
+K=kmeans(VEC,centers=N)
+pbmc@meta.data$kclust=K$cluster   
+
+
+
+pdf('KDD_CLUST.pdf',width=10,height=10)
+DimPlot(pbmc, reduction.use='umap', group.by='kclust', pt.size=0.1,label=T)+NoLegend()
+dev.off()
+
+
+
+
+
+
+#'Defa22','Defa24',
+
+
+
+pdf('KDD_GENE.pdf',width=10,height=10)
+markers.to.plot <- c('Lyz1','Defa17','Ang4','Mmp7')
+FeaturePlot(pbmc, features=markers.to.plot)
+
+markers.to.plot <- c('Ptprc','Cd44')
+FeaturePlot(pbmc, features=markers.to.plot)
+
+dev.off()
+
+markers.to.plot <- c('Ptprc')
+
+
+DotPlot(pbmc, features = rev(markers.to.plot), xlab='',ylab='',cols = c("blue", "red"), group.by='kclust',dot.scale = 8) + RotatedAxis()
+
+
+Idents(pbmc)=pbmc@meta.data$kclust
+
+cluster78.markers <- FindMarkers(pbmc, ident.1 = 78, min.pct = 0.5)
+head(cluster78.markers, n = 5)
+
+cluster95.markers <- FindMarkers(pbmc, ident.1 = 95, min.pct = 0.5)
+head(cluster95.markers, n = 5)
+
+
+Immune=c(31,86,74,38,30,94)
+Tuft=c(57)
+Goblet=c(77,34,80,49)
+Endocrine=c(95,78)
+
+
+
+END=c(34,176)
+MES=c(59)
+OL=c(23,20,187,294,123)
 
 
 
