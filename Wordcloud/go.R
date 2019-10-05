b572ba074c463296f198d42a6cd4b7e7dfe7e809
Zhang, Feng <jumphone@163.com>
2019-10-05 10:47:39 -0400

    Update go.R

--- Wordcloud/go.R
+++ Wordcloud/go.R
@@ -1,8 +1,22 @@
 #http://www.sthda.com/english/wiki/word-cloud-generator-in-r-one-killer-function-to-do-everything-you-need
 
-install.packages(c("tm", "SnowballC", "wordcloud", "RColorBrewer", "RCurl", "XML")
+#install.packages(c("tm", "SnowballC", "wordcloud", "RColorBrewer", "RCurl", "XML")
 
-rquery.wordcloud(x, type=c("text", "url", "file"), 
-        lang="english", excludeWords = NULL, 
-        textStemming = FALSE,  colorPalette="Dark2",
-        max.words=200)
+                 
+#source('https://raw.githubusercontent.com/jumphone/Bioinformatics/master/Wordcloud/rquery_wordcloud.R')
+                
+#rquery.wordcloud(x=as.character(rnorm(100)), type=c("text"), lang = "english", min.freq = 1,  max.words = 200)
+
+
+library(wordcloud)
+
+X=c('YES','OK','NO')
+F=c(5,4,3)
+COL=c('red','blue','black')
+set.seed(123)
+wordcloud(X,F, min.freq=0, max.words=3,
+            random.order=FALSE, rot.per=0.35, 
+            use.r.layout=FALSE, colors=COL,ordered.colors=TRUE)
+
+
+
