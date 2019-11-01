204fdd3e5416ccd9d2afab33a289bf53117e3776
Zhang, Feng <jumphone@163.com>
2019-11-01 11:34:45 -0400

    Create go_new_1101.R

---
+++ scRNAseq/HongFangZi/go_new_1101.R
@@ -0,0 +1,7 @@
+
+#setwd('/users/zha8dh/tianlab/HFZ')
+setwd('F:/Hongfangzi')
+
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+DATA=readRDS('./DATA.RDS')
+BATCH=readRDS('./BATCH.RDS')
