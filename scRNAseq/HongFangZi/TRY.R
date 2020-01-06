d2ce027d3a3bd2d5204658aed4efbfbaa50da9a3
Zhang, Feng <15110700005@fudan.edu.cn>
2020-01-06 10:15:23 -0500

    Update TRY.R

--- scRNAseq/HongFangZi/TRY.R
+++ scRNAseq/HongFangZi/TRY.R
@@ -1,1 +1,61 @@
 
+setwd('F:/Hongfangzi')
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+
+DATA=readRDS( file='./DATA.RDS')
+BATCH=readRDS( file='./BATCH.RDS')
+
+USED=which(BATCH %in% c('placenta0423.2','placenta508.1',
+                       'placenta508.2','placenta514.2',
+                       'placenta2018c', 'placenta2019c',
+                       'placenta20190402'
+                       ))
+
+
+
+
+DATA=DATA[,USED]
+
+library(Seurat)
+# DATA: Expression matrix. Rownames are gene names. Colnames are cell names.
+pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 5000)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 150)
+pbmc <- RunUMAP(pbmc, dims = 1:150)
+DimPlot(pbmc, reduction = "umap")
+
+VEC = pbmc@reductions$umap@cell.embeddings
+rownames(VEC) = colnames(pbmc)
+PCA = pbmc@reductions$pca@cell.embeddings
+
+PCA=vector.rankPCA(PCA)
+
+
+OUT=vector.buildGrid(VEC, N=30,SHOW=TRUE)
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=TRUE)
+
+saveRDS(pbmc,'pbmc_vector.RDS')
+
+
+FeaturePlot(pbmc,features='ACTA2')
+
+used_cell=vector.SeuratSelect(pbmc)
+
+
+
+
+# FB : ACTA2, COL1A1
+
+
+
+
+
+
