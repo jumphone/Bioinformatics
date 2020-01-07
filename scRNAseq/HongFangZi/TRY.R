f5adc9df21aceea0c88a154a9b5b49ddb0981c62
Zhang, Feng <15110700005@fudan.edu.cn>
2020-01-07 13:57:52 -0500

    Update TRY.R

--- scRNAseq/HongFangZi/TRY.R
+++ scRNAseq/HongFangZi/TRY.R
@@ -5,6 +5,80 @@ source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 DATA=readRDS( file='./DATA.RDS')
 BATCH=readRDS( file='./BATCH.RDS')
 
+USED=which(BATCH %in% c('decidua0117','decidua0417.2',
+                       'decidua2018c','decidua20190215',
+                       'decidua20190420', 'decidua2019c',
+                       'decidua508','decidua510'
+                       ))
+
+
+
+
+DATA=DATA[,USED]
+BATCH=BATCH[USED]
+
+mybeer=BEER(DATA, BATCH, GNUM=30, PCNUM=150, ROUND=1, GN=2000, SEED=1, COMBAT=TRUE, RMG=NULL)   
+
+
+# Check selected PCs
+PCUSE=mybeer$select
+COL=rep('black',length(mybeer$cor))
+COL[PCUSE]='red'
+plot(mybeer$cor,mybeer$lcor,pch=16,col=COL,
+    xlab='Rank Correlation',ylab='Linear Correlation',xlim=c(0,1),ylim=c(0,1))
+
+
+saveRDS(mybeer,'mybeer_decidua.RDS')
+
+
+
+
+PCUSE <- mybeer$select
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims = PCUSE, check_duplicates=FALSE)
+
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#######################
+setwd('F:/Hongfangzi')
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+
+DATA=readRDS( file='./DATA.RDS')
+BATCH=readRDS( file='./BATCH.RDS')
+
 USED=which(BATCH %in% c('placenta0423.2','placenta508.1',
                        'placenta508.2','placenta514.2',
                        'placenta2018c', 'placenta2019c',
@@ -45,9 +119,6 @@ OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
 OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=TRUE)
 Sys.time()
 
-
-
-
 FeaturePlot(pbmc,features='ACTA2')
 ################################################################################
 
@@ -71,6 +142,20 @@ USED=which(BATCH %in% c('decidua0117','decidua0417.2',
 DATA=DATA[,USED]
 BATCH=BATCH[USED]
 
+
+mybeer=BEER(DATA, BATCH, GNUM=30, PCNUM=150, ROUND=1, GN=5000, SEED=1, COMBAT=TRUE, RMG=NULL)   
+
+
+
+
+
+
+
+
+
+
+
+
 library(Seurat)
 # DATA: Expression matrix. Rownames are gene names. Colnames are cell names.
 pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
@@ -82,14 +167,25 @@ pbmc <- ScaleData(pbmc, features = all.genes)
 pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 150)
 pbmc <- RunUMAP(pbmc, dims = 1:150)
 DimPlot(pbmc, reduction = "umap")
-saveRDS(pbmc,'pbmc_placenta.RDS')
+
+saveRDS(pbmc,'pbmc_decidua.RDS')
 
 
 VEC = pbmc@reductions$umap@cell.embeddings
 rownames(VEC) = colnames(pbmc)
 PCA = pbmc@reductions$pca@cell.embeddings
 
+Sys.time()
+PCA=vector.rankPCA(PCA)
+OUT=vector.buildGrid(VEC, N=30,SHOW=TRUE)
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=TRUE)
+Sys.time()
 
+FeaturePlot(pbmc,features='DIO2')
 
 
 
