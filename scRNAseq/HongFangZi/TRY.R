f591501f62eac31602cfcc0371c5b2a307ad952b
Zhang, Feng <15110700005@fudan.edu.cn>
2020-01-08 16:42:18 -0500

    Update TRY.R

--- scRNAseq/HongFangZi/TRY.R
+++ scRNAseq/HongFangZi/TRY.R
@@ -22,7 +22,7 @@ BATCH=BATCH[USED]
 
 
 ##########################
-mybeer=BEER(DATA, BATCH, GNUM=30, PCNUM=150, ROUND=1, GN=2000, SEED=1, COMBAT=FALSE, RMG=NULL)   
+mybeer=BEER(DATA, BATCH, GNUM=30, PCNUM=150, ROUND=1, GN=2000, SEED=1, COMBAT=TRUE, RMG=NULL)   
 
 
 # Check selected PCs
@@ -39,10 +39,77 @@ saveRDS(mybeer,'mybeer_decidua.RDS')
 
 pbmc=mybeer$seurat
 PCUSE <- mybeer$select
+#pbmc=BEER.combat(pbmc) 
+#umap=BEER.bbknn(pbmc, PCUSE, NB=3, NT=10)
+#pbmc@reductions$umap@cell.embeddings=umap
+
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims = PCUSE, check_duplicates=FALSE,
+                n.neighbors = 20, min.dist=0.1)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1,label=F)
+
+FeaturePlot(pbmc,features='CD34',order=TRUE)
+FeaturePlot(pbmc,features='COL1A1',order=TRUE)
+FeaturePlot(pbmc,features='VIM',order=TRUE)
+
+
+saveRDS(pbmc,'pbmc_decidua.RDS')
+
+
+
+VEC = pbmc@reductions$umap@cell.embeddings
+rownames(VEC) = colnames(pbmc)
+PCA = pbmc@reductions$pca@cell.embeddings
+# Remove quantile-based colinearity among PCs (new feature in VECTOR 0.0.3):   
+PCA=vector.rankPCA(PCA)
+
+
+# Define pixel
+OUT=vector.buildGrid(VEC, N=30,SHOW=TRUE)
+
+# Build network
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+
+# Calculate Quantile Skewness (QS) score
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
+
+# Get pixel's QS score
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+
+# Find starting point
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+
+# Infer vector
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+FeaturePlot(pbmc,features='CCL5',order=TRUE)
+
+
 #pbmc=BEER.combat(pbmc)
-#PCA = pbmc@reductions$pca@cell.embeddings
-#PCA=vector.rankPCA(PCA)
-#pbmc@reductions$pca@cell.embeddings=PCA
+
+
+
 
 pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims = PCUSE, check_duplicates=FALSE,
                 n.neighbors = 20, min.dist=0.1)
@@ -73,9 +140,9 @@ TYPE[which(pbmc@meta.data$batch %in% c('decidua2019c',
 pbmc@meta.data$type=TYPE
 
 
-
-
 DimPlot(pbmc, reduction.use='umap', group.by='type', pt.size=0.1) 
+
+
 
 USED=vector.SeuratSelect(pbmc)
 pbmc=vector.SeuratAddMetaByCell(pbmc, USED)
@@ -123,6 +190,9 @@ OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=TRUE)
 
 
 
+#PCA = pbmc@reductions$pca@cell.embeddings
+#PCA=vector.rankPCA(PCA)
+#pbmc@reductions$pca@cell.embeddings=PCA
 
 
 
