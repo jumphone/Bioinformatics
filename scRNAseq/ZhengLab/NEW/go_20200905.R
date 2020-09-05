d7dec638cc63d7ef978d16b58be0414c06aea428
Zhang, Feng <15110700005@fudan.edu.cn>
2020-09-05 21:31:01 +0800

    Update go_20200905.R

--- scRNAseq/ZhengLab/NEW/go_20200905.R
+++ scRNAseq/ZhengLab/NEW/go_20200905.R
@@ -1,1 +1,9 @@
+setwd('F:/Zhenglab/NewZhengZhang/NEW_20200905')
+library(Seurat)
+pbmc=readRDS('../pbmc.final.RDS')
+
+wt.pbmc=readRDS('new.wt.pbmc.rds')
+ko.pbmc=readRDS('new.ko.pbmc.rds')
+
+HQRef=readRDS('HQRef.rds')
 
