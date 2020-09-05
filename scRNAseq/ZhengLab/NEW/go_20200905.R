bbba9106ebef9f0ee3c61e168a16e2f64a21a21d
Zhang, Feng <15110700005@fudan.edu.cn>
2020-09-05 22:09:45 +0800

    Update go_20200905.R

--- scRNAseq/ZhengLab/NEW/go_20200905.R
+++ scRNAseq/ZhengLab/NEW/go_20200905.R
@@ -17,9 +17,39 @@ out=.get_cor(exp_sc_mat, HQRef, method='spearman',CPU=4, print_step=10)
 sc_tag=.get_tag_max(out)
 
 ref_vec=pbmc@reductions$umap@cell.embeddings
+saveRDS(ref_vec, 'ref_vec.rds')
 
-out =.vec_projection(exp_sc_mat, sc_tag, exp_ref_mat, ref_tag, ref_vec, 
+
+
+pbmc=readRDS('../pbmc.final.RDS')
+VG=VariableFeatures(pbmc)
+saveRDS(VG, file='VG.rds')
+
+exp_ref_mat[which(rownames(exp_ref_mat) %in% VariableFeatures(pbmc)),]
+
+######################################
+setwd('F:/Zhenglab/NewZhengZhang/NEW_20200905')
+source('https://raw.githubusercontent.com/jumphone/scRef/master/scRef.R')
+
+library(Seurat)
+exp_ref_mat=readRDS('exp_ref_mat.rds')
+ref_vec=readRDS('ref_vec.rds')
+ref_tag=readRDS('ref_tag.rds')
+HQRef=readRDS('HQRef.rds')
+VG=readRDS('VG.rds')
+wt.pbmc=readRDS('new.wt.pbmc.rds')
+ko.pbmc=readRDS('new.ko.pbmc.rds')
+
+exp_sc_mat=as.matrix(wt.pbmc@assays$RNA@data)
+sc_tag=readRDS('wt.sc_tag.rds')
+
+out =.vec_projection(exp_sc_mat[which(rownames(exp_sc_mat) %in% VG),], sc_tag, exp_ref_mat[which(rownames(exp_ref_mat) %in% VG),], ref_tag, ref_vec, 
         method='spearman', nearest_cell=3, alpha=0.5, random_size=30, 
         random_seed=123, CPU=2, print_step=10)
+
+
+
+
+
 
 
