1576758830902ad7aaa8b2a039fe01b730e3404e
Zhang, Feng <15110700005@fudan.edu.cn>
2020-09-05 21:42:56 +0800

    Update go_20200905.R

--- scRNAseq/ZhengLab/NEW/go_20200905.R
+++ scRNAseq/ZhengLab/NEW/go_20200905.R
@@ -1,4 +1,6 @@
 setwd('F:/Zhenglab/NewZhengZhang/NEW_20200905')
+source('https://raw.githubusercontent.com/jumphone/scRef/master/scRef.R')
+
 library(Seurat)
 pbmc=readRDS('../pbmc.final.RDS')
 
@@ -6,4 +8,18 @@ wt.pbmc=readRDS('new.wt.pbmc.rds')
 ko.pbmc=readRDS('new.ko.pbmc.rds')
 
 HQRef=readRDS('HQRef.rds')
+ref_tag=cbind(colnames(pbmc),as.character(pbmc@meta.data$celltype))
+
+exp_ref_mat=as.matrix(pbmc@assays$RNA@data)
+exp_sc_mat=as.matrix(wt.pbmc@assays$RNA@data)
+
+out=.get_cor(exp_sc_mat, HQRef, method='spearman',CPU=4, print_step=10)
+sc_tag=.get_tag_max(out)
+
+ref_vec=pbmc@reductions$umap@cell.embeddings
+
+out =.vec_projection(exp_sc_mat, sc_tag, exp_ref_mat, ref_tag, ref_vec, 
+        method='spearman', nearest_cell=3, alpha=0.5, random_size=30, 
+        random_seed=123, CPU=2, print_step=10)
+
 
